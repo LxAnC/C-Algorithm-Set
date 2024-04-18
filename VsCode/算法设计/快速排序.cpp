4be4eaaaeba3b15594cf1bad655dc6c2b1a192c0
LuXuAn <5001363@qq.com>
2024-04-18 14:56:39 +0800

    FIx

--- VsCode/算法设计/快速排序.cpp
+++ VsCode/算法设计/快速排序.cpp
@@ -5,28 +5,51 @@
  * @date: 2024-04-18 
  */
 //快排
-
-#include <bits/stdc++.h>
-
-using namespace std;
-
-void quicksort()
-{
-  int a[]={3,1,2,4,5,6,8,100,2};
-  int n=9,left,right,key,flag=1;
-  while(flag==1)
-  {
-    flag=0;
-    while()
-  }
-
-  // cout<<n;
+#include <iostream>
+#include <vector>
+void quickSort(std::vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
 }
 
-int main()
-{
-   int t=1;
-   while(t--)
-   quicksort();
-   return 0;
+int partition(std::vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1); 
+
+        if (arr[j] <= pivot) {
+            i++; 
+            std::swap(arr[i], arr[j]); 
+        }
+    
+    std::swap(arr[i + 1], arr[high]);
+    return (i + 1); // 返回分区索引
+}
+
+// 快速排序的辅助函数，用于调用快速排序
+void quickSortWrapper(std::vector<int>& arr) {
+    quickSort(arr, 0, arr.size() - 1);
+}
+
+// 打印数组的函数
+void printArray(const std::vector<int>& arr) {
+    for (int i : arr) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    std::vector<int> arr = {10, 7, 8, 9, 1, 5};
+    std::cout << "原数组: "<<std::endl;
+    printArray(arr);
+
+    quickSortWrapper(arr);
+
+    std::cout << "排序后"<<std::abort;
+    printArray(arr);
+
+    return 0;
 }
