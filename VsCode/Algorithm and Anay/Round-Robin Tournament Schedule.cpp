a2a517a17ecbcac61ae438ae0c741bf971793da5
LuXuAn <5001363@qq.com>
2024-04-18 16:41:13 +0800

    循环赛日程表算法更新

--- VsCode/Algorithm and Anay/Round-Robin Tournament Schedule.cpp
+++ VsCode/Algorithm and Anay/Round-Robin Tournament Schedule.cpp
@@ -4,59 +4,61 @@
  * @author: LxAnC
  * @date: 2024-04-18 
  */
+#include <iostream>
+#include <cstring>
 #include <bits/stdc++.h>
+using namespace std;
 
-using namespace std;  
-int k;
-bool  ak[100000001];
-vector<vector<bool>>bk(k+1,vector<bool>(k+1,false));
-void printarry(vector<vector<int>> &n,long long k)
+int Table(int k,int a[100][100])
 {
-    for(int m=1;m<=k;m++)
-{       cout<<m<<' ';
-    for(int i=1;i<=k;i++)     
-      cout<<n[i][m]<<' '; 
-       cout<<endl;}
+    //求解2^1个选手比赛日程，得到左上角元素
+    a[1][1] = 1;
+    a[1][2] = 2;
+    a[2][1] = 2;
+    a[2][2] = 1;
+    if(k>1)
+{        for(int t=1; t<k; t++)
+        {
+            for(int i=1+pow(2.0,t); i<=pow(2.0,t+1); i++)
+            {
+                for(int j=1; j<=pow(2.0,t); j++)
+                {
+                    a[i][j] = a[i-(int)pow(2.0,t)][j] + (int)pow(2.0,t);
+                }
+            }
+            for(int i=1; i<=pow(2.0,t); i++)
+            {
+                for(int j=1+pow(2.0,t); j<=pow(2.0,t+1); j++)
+                {
+                    a[i][j] = a[j][i];
+                }
+            }
+            for(int i=1+pow(2.0,t); i<=pow(2.0,t+1); i++)
+            {
+                for(int j=1+pow(2.0,t); j<=pow(2.0,t+1); j++)
+                {
+                    a[i][j] = a[i-(int)pow(2.0,t)][j-(int)pow(2.0,t)];
+                }
+            }
 
+        }}
+    else    ;
+    return 0;
 }
-void solve()
-{ 
-  int flag;
-  cin>>k;
-  k=pow(2,k);    
-  vector<vector<bool>>ak(k+1,vector<bool>(k+1,false));
-  vector<vector<int>>n(k+1,vector<int>(k+1,0));
-  for(int i=1;i<=k;i++)
- { 
-
-    for(int j=1;j<=k;j++)
-    for(int m=1;m<=k;m++)
-    {
-      if(m!=j&&ak[m][j]==false&&ak[j][m]==false&&n[j][i]==0)
-      {
-      ak[m][j]=ak[j][m]=true;
-      n[j][i]=m;
-      n[m][i]=j;
-      flag=1;
-      }
-      if(flag==1)
-      {flag=0;
-      break;}
-    }
-  
-      
-}
-  
-  printarry(n,k);
-}
-
 int main()
 {
-   int t,k;
-t=1;
-   while(t--)
-   {
-    solve();
-   }
-   return 0;
+    int k;
+    int a[100][100];
+    cin>>k;
+    double n=pow(2.0,k);
+    Table(k,a);
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cout << a[i][j] << "　 ";
+        }
+        cout << endl<<endl;
+    }
+    return 0;
 }
