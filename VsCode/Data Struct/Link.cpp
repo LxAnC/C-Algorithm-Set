d05f2801a2612b5a413640bb8c5d775e514340ed
LuXuAn <5001363@qq.com>
2024-04-18 18:05:44 +0800

    手写链表

--- VsCode/Data Struct/Link.cpp
+++ VsCode/Data Struct/Link.cpp
@@ -4,12 +4,24 @@
  * @author: LxAnC
  * @date: 2024-04-18 
  */
+//手搓一个单链表
 #include <bits/stdc++.h>
 
 using namespace std;
 
+typedef struct LinkNode{
+  int data;
+   LinkNode *next;
+}*a;
 int main()
 {
-   
+   //构造一个单链表
+  a n=new LinkNode;
+  a n1=new LinkNode;
+  n1->data=3;
+  n->data=2;
+  n->next=n1;
+
+   cout<<n->next->data<<endl;
    return 0;
 }
