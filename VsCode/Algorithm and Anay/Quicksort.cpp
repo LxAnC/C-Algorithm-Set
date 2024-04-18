20d2b98da583ff07cad4f8da98da299acc357cc8
LuXuAn <5001363@qq.com>
2024-04-18 15:12:41 +0800

    完善了

--- VsCode/Algorithm and Anay/Quicksort.cpp
+++ VsCode/Algorithm and Anay/Quicksort.cpp
@@ -1,46 +1,56 @@
 /**
  * Copyright © 2024 LxAnC. All rights reserved.
- * 
+ *
  * @author: LxAnC
- * @date: 2024-04-18 
+ * @date: 2024-04-18
  */
-//快排
+// 快排
 #include <iostream>
 #include <vector>
-int partition(std::vector<int>& arr, int low, int high) {
+using namespace std;
+int partition(vector<int> &arr, int low, int high)
+{
 
     int pivot = arr[high];
-    int i = (low - 1); 
+    int i = (low - 1);
 
-    for (int j = low; j <= high - 1; j++) {
-        if (arr[j] <= pivot) {
-            i++; 
-            std::swap(arr[i], arr[j]); 
+    for (int j = low; j <= high - 1; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            i++;
+        swap(arr[i], arr[j]);
         }
     }
-    std::swap(arr[i + 1], arr[high]);
-    return (i + 1); 
+    swap(arr[i + 1], arr[high]);
+    return (i + 1);
 }
 
-void quickSort(std::vector<int>& arr, int low, int high) {
-    if (low < high) {
+void quickSort(vector<int> &arr, int low, int high)
+{
+    if (low < high)
+    {
         int pi = partition(arr, low, high);
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
     }
 }
-void quickSortWrapper(std::vector<int>& arr) {
+void quickSortWrapper(vector<int> &arr)
+{
     quickSort(arr, 0, arr.size() - 1);
 }
-void printArray(const std::vector<int>& arr) {
-    for (int i : arr) {
-        std::cout << i << " ";
+void printArray(const vector<int> &arr)
+{
+    for (int i : arr)
+    {
+        cout << i << " ";
     }
-    std::cout << std::endl;
+    cout << endl;
 }
 
-int main() {
-    std::vector<int> arr = {10, 7, 8, 9, 1, 5};
+int main()
+{
+    vector<int> arr = {10, 7, 8, 9, 1, 5};
     printArray(arr);
     quickSortWrapper(arr);
     printArray(arr);
