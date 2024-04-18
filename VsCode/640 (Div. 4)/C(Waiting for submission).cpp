5cb01465cd497ba0833fb6e1ab20bf02ea87619e
LuXuAn <5001363@qq.com>
2024-04-18 08:34:46 +0800

    C尝试

--- VsCode/640 (Div. 4)/C(Waiting for submission).cpp
+++ VsCode/640 (Div. 4)/C(Waiting for submission).cpp
@@ -8,8 +8,22 @@
 
 using namespace std;
 
+void solve()
+{
+   long long n,k,ret=0;
+   cin>>n>>k;
+   for(int i=n;1;i++)
+   {
+
+   }
+}
 int main()
 {
-   
+   int t=1;
+   // cin>>t;
+   while(t--)
+   {
+      solve();
+   }
    return 0;
 }
