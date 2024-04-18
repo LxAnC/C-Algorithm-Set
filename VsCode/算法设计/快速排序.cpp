5821a1a86af7455de95725677b65b50ea743d64f
LuXuAn <5001363@qq.com>
2024-04-18 14:51:43 +0800

    复习一下快排和准备D题

---
+++ VsCode/算法设计/快速排序.cpp
@@ -0,0 +1,32 @@
+/**
+ * Copyright © 2024 LxAnC. All rights reserved.
+ * 
+ * @author: LxAnC
+ * @date: 2024-04-18 
+ */
+//快排
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void quicksort()
+{
+  int a[]={3,1,2,4,5,6,8,100,2};
+  int n=9,left,right,key,flag=1;
+  while(flag==1)
+  {
+    flag=0;
+    while()
+  }
+
+  // cout<<n;
+}
+
+int main()
+{
+   int t=1;
+   while(t--)
+   quicksort();
+   return 0;
+}
