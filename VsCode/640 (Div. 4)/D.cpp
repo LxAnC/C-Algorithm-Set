22f54f736a4f23d7913f0b27c7c2457163bc2d3a
LuXuAn <5001363@qq.com>
2024-04-18 17:49:51 +0800

    尝试Div4D题

--- VsCode/640 (Div. 4)/D.cpp
+++ VsCode/640 (Div. 4)/D.cpp
@@ -8,8 +8,25 @@
 
 using namespace std;
 
+void solve()
+{
+   int n;
+   int suma=0,sumb=0,total=0;
+   vector<int>c(n);
+   for(int i=0;i<n;i++)
+   cin>>c[i],total+=c[i];
+   int curcandy=0,curcandy2;
+   int left=0,right=c.size()-1;
+   
+      
+}
 int main()
 {
-   
+   int t;
+   cin>>t;
+   while(t--)
+   {
+      solve();
+   }
    return 0;
 }
