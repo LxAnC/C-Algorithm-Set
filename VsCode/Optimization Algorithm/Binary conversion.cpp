30cc0a17c0bc0ec2636a63ad27ce2ab611deb885
LuXuAn <5001363@qq.com>
2024-04-19 10:11:28 +0800

    复习一下进制转换

---
+++ VsCode/Optimization Algorithm/Binary conversion.cpp
@@ -0,0 +1,23 @@
+/**
+ * Copyright © 2024 LxAnC. All rights reserved.
+ *
+ * @author: LxAnC
+ * @date: 2024-04-19
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  char ch[20000];
+  string s;
+  int n, x;
+  getline(cin,s);
+
+  strtol(ch,x,2);
+  // itoa(x, ch, 2);
+  // for (int i = 0; ch[i] != '\0'; i++)  //测试十进制转任意进制
+  //   cout << ch[i];
+  return 0;
+}
