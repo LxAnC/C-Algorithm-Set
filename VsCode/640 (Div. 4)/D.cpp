03ea6c21d59ac03de873aad39bf0eacaebd762b0
LuXuAn <5001363@qq.com>
2024-04-20 21:41:27 +0800

    为什么错啊

--- VsCode/640 (Div. 4)/D.cpp
+++ VsCode/640 (Div. 4)/D.cpp
@@ -44,29 +44,37 @@ using namespace std;
 // 还是有些案例过不了
 void solve()
 {
-   int n,a,b,c=0,suma=0,sumb=0,step=0;
-   cin>>n;
-   vector<int>s(n);
-   for(int i=0;i<n;i++)
-   cin>>s[i];
-   int l=0,r=n-1;
-   while(suma+sumb!=accumulate(s.begin(),s.end(),0))
+   int n, a, b, c = 0, suma = 0, sumb = 0, step = 0;
+   cin >> n;
+   vector<int> s(n);
+   for (int i = 0; i < n; i++)
+      cin >> s[i];
+   int l = 0, r = n - 1;
+   while (suma + sumb != accumulate(s.begin(), s.end(), 0))
    {
-      a=0,b=0;
-      while(a<=c&&left<=right)
+      a = 0, b = 0;
+      while (a <= c && left <= right)
       {
-         a+=s[l++];
+         a += s[l++];
       }
-      if(a!=0)
-      step++,c=a,suma+=a;
-      while(b<=c&&right>=left)
+      if (a != 0)
       {
-         b+=s[r--];
+         step++;
+         c = a;
+         suma += a;
       }
-      if(b)
-      step++,c=b,sumb+=b;
+      while (b <= c && right >= left)
+      {
+         b += s[r--];
+      }
+      if (b)
+      {
+         step++;
+         c = b;
+         sumb += b;
+      }
    }
-   cout<<step<<' '<<suma<<' '<<sumb<<endl;
+   cout << step << ' ' << suma << ' ' << sumb << endl;
 }
 int main()
 {
