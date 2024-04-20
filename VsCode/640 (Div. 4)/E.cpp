7e649fc4a50d3d0fe76bae84280b4dda51870cbe
LuXuAn <5001363@qq.com>
2024-04-20 22:13:19 +0800

    Try One

--- VsCode/640 (Div. 4)/E.cpp
+++ VsCode/640 (Div. 4)/E.cpp
@@ -10,29 +10,37 @@ using namespace std;
 
 void solve()
 {
-  int n,ret=0,sum;
+  int n, ret = 0, sum;
   cin >> n;
   vector<int> s(n);
   for (int i = 0; i < n; i++)
   {
     cin >> s[i];
   }
-  for(int i=0;i<n;i++)
+  for (int i = 0; i < n; i++)
   {
-     sum=0;
-    for(int j=0;j<i;j++)
+    for (int j = 0; j < n; j++)
     {
-        if(sum<s[i])
-        {
-          sum+=s[j];
-        }
-        else if(sum==s[i])
-       { ret++;break;}
-       else
-       break;
+      sum = 0;
+      while (sum < s[i])
+      {
+        if(i==j)
+       { j++;break;}
+        sum+=s[j++];
+
+      }
+      if(sum>s[i])
+      {
+        continue;
+      }
+      if(sum==s[i])
+      {
+        ret++;
+        break;
+      }
     }
   }
-  cout<<ret<<endl;
+  cout << ret << endl;
 }
 int main()
 {
