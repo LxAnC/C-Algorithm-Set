4e2790e2791ffbc08cdbc672df9b71d058c07da3
LuXuAn <5001363@qq.com>
2024-04-20 13:25:18 +0800

    模拟一下,锻炼一下思维

--- VsCode/640 (Div. 4)/D.cpp
+++ VsCode/640 (Div. 4)/D.cpp
@@ -1,8 +1,8 @@
 /**
  * Copyright © 2024 LxAnC. All rights reserved.
- * 
+ *
  * @author: LxAnC
- * @date: 2024-04-18 
+ * @date: 2024-04-18
  */
 #include <bits/stdc++.h>
 
@@ -10,21 +10,42 @@ using namespace std;
 
 void solve()
 {
-   int n;
-   int suma=0,sumb=0,total=0;
-   vector<int>c(n);
-   for(int i=0;i<n;i++)
-   cin>>c[i],total+=c[i];
-   int curcandy=0,curcandy2;
-   int left=0,right=c.size()-1;
-   
-      
+   int num,ret=0;
+   cin >> num;
+   vector<int> c(num);
+   for (int i = 0; i < num; i++)
+      cin >> c[i];
+   int count_candy = accumulate(c.begin(), c.end(), 0);
+   int left = 0, right = num - 1, current_candy = 0, left2 = 0, right2 = 0;
+
+   while (count_candy > current_candy && left <= right)
+   {
+      // 统计每一轮吃的糖果数
+      int currl = 0, currr = 0;
+      // 左边的糖果
+      while (currl <= current_candy&&left<=right) // 判断当前糖果是否满足要求
+      {
+         currl += c[left++];
+      }
+      count_candy -= currl; // 总数减去左边吃的糖果;
+      left2 += currl;       // 统计左边吃过的糖果
+      current_candy = currl;
+      while (currr <= current_candy&&right>=left)
+      {
+         currr += c[right--];
+      }
+      count_candy -= currr;
+      right2 += currr; // 统计左边吃过的糖果
+      current_candy = currr;
+      ret++;
+   }
+   cout<<ret<<" "<<left2<<" "<<right2<<endl;
 }
 int main()
 {
    int t;
-   cin>>t;
-   while(t--)
+   cin >> t;
+   while (t--)
    {
       solve();
    }
