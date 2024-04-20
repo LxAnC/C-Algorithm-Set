95cdc574a642b819c682246910fe94c4560a87c4
LuXuAn <5001363@qq.com>
2024-04-20 21:50:35 +0800

    yeahyeahyeah完美通过

--- VsCode/640 (Div. 4)/D.cpp
+++ VsCode/640 (Div. 4)/D.cpp
@@ -7,44 +7,9 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
-// #include<bits/stdc++.h>
-// using namespace std;
-// int t,n,a[1001];
-// int main(){
-// 	cin>>t;
-// 	while(t--){
-// 		cin>>n;
-// 		for(int i=1;i<=n;i++)cin>>a[i];
-// 		int l=2,r=n,slnow=a[1],srnow=0,sl=a[1],sr=0,step=1;
-// 		bool flag=1;
-// 		while(l<=r){
-// 			step++;
-// 			if(flag){
-// 				while(l<=r&&srnow<=slnow){
-// 					srnow+=a[r];
-// 					sr+=a[r];
-// 					r--;
-// 				}
-// 				flag=0,slnow=0;
-// 			}
-// 			else{
-// 				while(l<=r&&slnow<=srnow){
-// 					slnow+=a[l];
-// 					sl+=a[l];
-// 					l++;
-// 				}
-// 				flag=1,srnow=0;
-// 			}
-// 		}
-// 		cout<<step<<" "<<sl<<" "<<sr<<endl;
-// 	}
-// 	return 0;
-// }
-// 还是有些案例过不了
 void solve()
 {
-   int n, a, b, c = 0, suma = 0, sumb = 0, step = 0;
+   int n, c = 0, suma = 0, sumb = 0, step = 0;
    cin >> n;
    vector<int> s(n);
    for (int i = 0; i < n; i++)
@@ -52,8 +17,8 @@ void solve()
    int l = 0, r = n - 1;
    while (suma + sumb != accumulate(s.begin(), s.end(), 0))
    {
-      a = 0, b = 0;
-      while (a <= c && left <= right)
+      int a = 0, b = 0;
+      while (a <= c && l <= r)
       {
          a += s[l++];
       }
@@ -63,7 +28,7 @@ void solve()
          c = a;
          suma += a;
       }
-      while (b <= c && right >= left)
+      while (b <= c && r >= l)
       {
          b += s[r--];
       }
