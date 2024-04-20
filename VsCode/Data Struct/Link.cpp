1ea00ac5d83359f5ed69686be59ce7ce12933d5b
LuXuAn <5001363@qq.com>
2024-04-20 10:47:19 +0800

    学习数据结构

--- VsCode/Data Struct/Link.cpp
+++ VsCode/Data Struct/Link.cpp
@@ -1,51 +1,69 @@
 /**
  * Copyright © 2024 LxAnC. All rights reserved.
- * 
+ *
  * @author: LxAnC
- * @date: 2024-04-18 
+ * @date: 2024-04-18
  */
-//手搓一个单链表
+// 手搓一个单链表
 #include <bits/stdc++.h>
 // typedef  i;
 using namespace std;
-//定义数据结构
-typedef struct LinkNode{
+// 定义数据结构
+typedef struct LinkNode
+{
    int data;
    struct LinkNode *next;
-}*Linknode;
+} *Linklist, LinkNode;
+void PrintList(LinkNode *p)
+{  
+   if(p==nullptr)
+   {
+      cout<<"为空,打印失败"<<endl;
+      return ;
+   }
+   LinkNode *a=new LinkNode;
+   a=p->next;
+   cout<<"打印如下"<<endl;
+   while(a!=nullptr)
+   {
+      cout<<a->data<<' ';
+      a=a->next;
+   }
 
-void printLink(LinkNode* &a)
-{
-  Linknode p=new LinkNode;
-  Linknode ptr=a->next;
-  while(ptr!=nullptr)
-  {
-    cout<<ptr->data<<' ';
-    ptr=ptr->next;
-  }
 }
- LinkNode CreateLink()
- {
-    LinkNode* head=nullptr;
-    LinkNode* tail=nullptr;
-    int x;
-    while(cin>>x)
-    {
-        Linknode a1=new LinkNode;
-        if(a1!=nullptr)
-       { a1->data=x;
-        a1->next=nullptr;}
-        tail->next=a1;
-        tail=a1;
-    }
-    printLink()
- }
-
-
+void InsertNode(LinkNode *p)
+{
+   int x,n;
+   cout<<"请输入插入的数字个数";
+   cin>>n;
+   while(n--)
+   {LinkNode *new1 = new LinkNode;
+   cin>>x;
+   new1->data=x;
+   new1->next=nullptr;
+   p->next=new1;
+   p=p->next;
+   }
+}
+LinkNode *CreateLink()
+{
+   LinkNode *L = new LinkNode;
+   if (L == nullptr)
+      cout << "分配错误" << endl;
+   else
+   {
+      cout << "分配成功" << endl;
+      
+   }
+   L->next=nullptr;
+   return L;
+}
 
 int main()
 {
-   //构造一个单链表
-    CreateLink();
+   // 构造一个单链表
+   LinkNode *p = CreateLink();
+   InsertNode(p);
+   PrintList(p);
    return 0;
 }
