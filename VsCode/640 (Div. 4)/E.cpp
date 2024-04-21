343ff5e2b8d38db7bbb626b468644efc8375a0f8
LuXuAn <5001363@qq.com>
2024-04-21 11:15:19 +0800

    尝试打表

--- VsCode/640 (Div. 4)/E.cpp
+++ VsCode/640 (Div. 4)/E.cpp
@@ -7,40 +7,42 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-void solve()
-{
-  int n, ret = 0, sum;
-  cin >> n;
-  vector<int> s(n);
-  for (int i = 0; i < n; i++)
-  {
-    cin >> s[i];
-  }
-  for (int i = 0; i < n; i++)
-  {
-    for (int j = 0; j < n; j++)
-    {
-      sum = 0;
-      int b=j;
-      while (sum < s[i]&&b<n)
-      {
-        if(i==b||s[i]==s[b])
-       { b++;break;}
-        sum+=s[b++];
-
-      }
-      if(sum>s[i])
-      {
-        continue;
-      }
-      if(sum==s[i])
-      {
-        ret++;
-        break;
-      }
-    }
-  }
-  cout << ret << endl;
+// void solve()
+// {
+//   int n, ret = 0, sum;
+//   cin >> n;
+//   vector<int> s(n);
+//   for (int i = 0; i < n; i++)
+//   {
+//     cin >> s[i];
+//   }
+//   for (int i = 0; i < n; i++)
+//   {
+//     for (int j = 0; j < n; j++)
+//     {
+//       sum = 0;
+//       int b=j;
+//       while (sum < s[i]&&b<n)
+//       {
+//         if(i==b||s[i]==s[b])
+//        { b++;break;}
+//         sum+=s[b++];
+//       }
+//       if(sum>s[i])
+//       {
+//         continue;
+//       }
+//       if(sum==s[i])
+//       {
+//         ret++;
+//         break;
+//       }
+//     }
+//   }
+//   cout << ret << endl;
+// }
+void solve(){
+  
 }
 int main()
 {
