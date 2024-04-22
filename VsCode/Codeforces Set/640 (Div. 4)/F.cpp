4581344cfaaf246f1ac77bc07f63f60eb31d0698
LuXuAn <5001363@qq.com>
2024-04-22 23:01:09 +0800

    哇始终过不了

--- VsCode/Codeforces Set/640 (Div. 4)/F.cpp
+++ VsCode/Codeforces Set/640 (Div. 4)/F.cpp
@@ -12,61 +12,7 @@ void solve()
 {
   int n,m,k,flag3,flag2,flag1;//分别表示00,10/01,11的个数
   cin>>n>>m>>k;
-  flag1=n;
-  flag2=m;
-  flag3=k;
-  string ret="";
-  do
-  {
-    ret+="1";
-  } while (k--);
-  if(flag3)
-  {
-    while(m--)
-    {
-       char ch=ret.back()=='1'?'0':'1';
-       if(ch=='0'&&n)
-       {
-        ret+="0";
-        n--;
-       }
-       ret+=ch;
-    }
-  }
-  else{
-    ret="0";
-    while(m--)
-    {
-       char ch=ret.back()=='1'?'0':'1';
-       if(ch=='0'&&n)
-       {
-        ret+="0";
-        n--;
-       }
-       ret+=ch;
-    }
-  }
-       int b=2; 
-  while(n)
-  {
-    if(ret[0]=='0')
-    ret="0"+ret;
-    else if(ret.back()=='0')
-    ret+="0";
-    else
-    {
-
-      while(b<ret.size())
-      {
-        if(ret[b-1]=='0'&&ret[b]=='0'&&n)
-        {ret.insert(b-1,"0");
-        n--;}
-        b++;
-        
-      }
-    }
-    n--;
-  }
+  string ret;
   cout<<ret<<endl;
 }
 int main()
