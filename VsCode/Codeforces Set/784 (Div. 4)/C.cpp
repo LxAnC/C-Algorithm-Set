c84f68d35109574fc05ec09c1f23a68d8e9918be
LuXuAn <5001363@qq.com>
2024-04-23 15:28:03 +0800

    换个意思做.很简单了

--- VsCode/Codeforces Set/784 (Div. 4)/C.cpp
+++ VsCode/Codeforces Set/784 (Div. 4)/C.cpp
@@ -1,15 +1,42 @@
 /**
  * Copyright © 2024 LxAnC. All rights reserved.
- * 
+ *
  * @author: LxAnC
- * @date: 2024-04-23 
+ * @date: 2024-04-23
  */
 #include <bits/stdc++.h>
 
 using namespace std;
-
+void solve()
+{
+  int n, x, k, m;
+  cin >> n;
+  for (int i = 0; i < n; i++)
+  {
+    cin >> x;
+    if (i == 0)
+      k = x & 1;
+    if (i == 1)
+      m = x & 1;
+    if (i & 1)
+    {
+      if (x & 1 != k)
+        {cout << "NO" << endl;return ;}
+    }
+    else
+    {
+      if (x & 1 != m)
+        {cout << "NO" << endl;return ;}
+    }
+  }
+  cout<<"YES"<<endl;
+}
 int main()
 {
-   
-   return 0;
+  int t;
+  while (t--)
+  {
+    solve();
+  }
+  return 0;
 }
