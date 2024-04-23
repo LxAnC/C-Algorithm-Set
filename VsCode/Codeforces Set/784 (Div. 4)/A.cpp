b4a2106c80c7300e5a6bdbe39122f2d496b27d25
LuXuAn <5001363@qq.com>
2024-04-23 14:42:19 +0800

    做一下新的Div4提升一下代码水平

---
+++ VsCode/Codeforces Set/784 (Div. 4)/A.cpp
@@ -0,0 +1,15 @@
+/**
+ * Copyright © 2024 LxAnC. All rights reserved.
+ * 
+ * @author: LxAnC
+ * @date: 2024-04-23 
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+// https://codeforces.com/contest/1669
+int main()
+{
+   
+   return 0;
+}
