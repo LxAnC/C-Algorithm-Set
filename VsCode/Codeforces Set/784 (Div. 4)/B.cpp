85bf86e7332fb246a79f8cc03199671fda49d7b5
LuXuAn <5001363@qq.com>
2024-04-23 14:54:36 +0800

    很简单的B题

---
+++ VsCode/Codeforces Set/784 (Div. 4)/B.cpp
@@ -0,0 +1,30 @@
+/**
+ * Copyright © 2024 LxAnC. All rights reserved.
+ * 
+ * @author: LxAnC
+ * @date: 2024-04-23 
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve()
+{
+  int n ;
+  unordered_map<int>hash;
+  cin>>n;
+  if(n<3)
+  {cout<<-1;
+  return ;}
+  
+}
+int main()
+{
+   int t;
+   cin>>t;
+   while(t--)
+   {
+    solve();
+   }
+   return 0;
+}
