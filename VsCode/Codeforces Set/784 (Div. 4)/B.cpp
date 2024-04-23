b0ae1607f545930b603be76ea0a22e99319d4e20
LuXuAn <5001363@qq.com>
2024-04-23 15:03:15 +0800

    火速提交

--- VsCode/Codeforces Set/784 (Div. 4)/B.cpp
+++ VsCode/Codeforces Set/784 (Div. 4)/B.cpp
@@ -1,30 +1,39 @@
 /**
  * Copyright © 2024 LxAnC. All rights reserved.
- * 
+ *
  * @author: LxAnC
- * @date: 2024-04-23 
+ * @date: 2024-04-23
  */
 #include <bits/stdc++.h>
-
 using namespace std;
-
 void solve()
 {
-  int n ;
-  unordered_map<int>hash;
-  cin>>n;
-  if(n<3)
-  {cout<<-1;
-  return ;}
-  
+  int n, x;
+  int hash[200001] = {0};
+  cin >> n;
+  if (n < 3)
+  {
+    cout << -1;
+    return;
+  }
+  for (int i = 0; i < n; i++)
+  {
+    cin >> x;
+    hash[x] += 1;
+    if (hash[x] == 3)
+    {
+      cout << x;
+      return;
+    }
+  }
 }
 int main()
 {
-   int t;
-   cin>>t;
-   while(t--)
-   {
+  int t;
+  cin >> t;
+  while (t--)
+  {
     solve();
-   }
-   return 0;
+  }
+  return 0;
 }
