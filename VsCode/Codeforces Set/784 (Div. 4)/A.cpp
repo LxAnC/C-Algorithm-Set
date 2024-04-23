2ba96e11ce1dd61b632fe12d3a1fbf2850efc2da
LuXuAn <5001363@qq.com>
2024-04-23 14:47:13 +0800

    AC

--- VsCode/Codeforces Set/784 (Div. 4)/A.cpp
+++ VsCode/Codeforces Set/784 (Div. 4)/A.cpp
@@ -1,15 +1,35 @@
 /**
  * Copyright © 2024 LxAnC. All rights reserved.
- * 
+ *
  * @author: LxAnC
- * @date: 2024-04-23 
+ * @date: 2024-04-23
  */
 #include <bits/stdc++.h>
 
 using namespace std;
 // https://codeforces.com/contest/1669
+void solve()
+{
+  int n;
+  cin >> n;
+  if (n >= 1900)
+    cout << "Division 1" << endl;
+  else if (n >= 1600)
+    cout << "Division 2" << endl;
+  else if (n >= 1400)
+    cout << "Division 3" << endl;
+  else
+  {
+    cout << "Division 4" << endl;
+  }
+}
 int main()
 {
-   
-   return 0;
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    solve();
+  }
+  return 0;
 }
