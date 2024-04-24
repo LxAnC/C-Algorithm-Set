0a8082966d8f3b8959c1260daaebd90a88e7b626
LuXuAn <5001363@qq.com>
2024-04-24 16:42:34 +0800

    做一下算法设计的作业

---
+++ VsCode/Algorithm and Anay/longest common subsequence.cpp
@@ -0,0 +1,27 @@
+/**
+ * Copyright © 2024 LxAnC. All rights reserved.
+ * 
+ * @author: LxAnC
+ * @date: 2024-04-24 
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+int a[100005],b[100005];
+//输入两个数组,然后输出他们的最长公共子序列
+
+void solve()
+{
+  int n;
+  while()
+}
+int main()
+{
+   int t;
+   cin>>t;
+   while(t--)
+   {
+    solve();
+   }
+   return 0;
+}
