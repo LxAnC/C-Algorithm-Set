aaff60db9cbf6e74ece0f4a8c05cff3080d0c0b4
LuXuAn <5001363@qq.com>
2024-04-24 15:17:30 +0800

    分析一下

--- VsCode/Codeforces Set/784 (Div. 4)/D.cpp
+++ VsCode/Codeforces Set/784 (Div. 4)/D.cpp
@@ -4,35 +4,38 @@
  * @author: LxAnC
  * @date: 2024-04-24
  */
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int n,t;
+int n, t;
 string s;
 
 int main()
 {
-	cin>>t; 
-	
-	while(t--)
-	{
-		cin>>n>>s;
-		bool flag = true;
-		for(int i = 0;i < s.size();i++)
-		{
-			if(s[i] != 'W') 
-			{
-				int j = i;
-				while(s[j] == s[i] && j < s.size()) j++;
-				if((j == s.size() || s[j] == 'W') && (i == 0 || s[i-1] == 'W'))
-				{
-					flag = false;
-					break;	
-				}	
-				i = max(i,j-1);		
-			}
-		}
-		if(flag)puts("YES");
-		else puts("NO");
-	}
-} 
+  cin >> t;
+
+  while (t--)
+  {
+    cin >> n >> s;
+    bool flag = true;
+    for (int i = 0; i < s.size(); i++)
+    {
+      if (s[i] != 'W')
+      {
+        int j = i; // 记录当前坐标
+        while (s[j] == s[i] && j < s.size())//特判是不是有一模一样的全字符
+          j++;
+        if ((j == s.size() || s[j] == 'W') && (i == 0 || s[i - 1] == 'W'))//前一段判断表示
+        {
+          flag = false;
+          break;
+        }
+        i = max(i, j - 1);//优化algorithm
+      }
+    }
+    if (flag)
+      puts("YES");
+    else
+      puts("NO");
+  }
+}
