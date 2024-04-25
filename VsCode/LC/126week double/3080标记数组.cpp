61558fff1ed4db8bf5697b69f7f3ff1c23473470
LuXuAn <5001363@qq.com>
2024-04-25 22:25:08 +0800

    灵神的代码,明天看

--- VsCode/LC/126week double/3080标记数组.cpp
+++ VsCode/LC/126week double/3080标记数组.cpp
@@ -1,41 +1,28 @@
-/**
- * Copyright © 2024 LxAnC. All rights reserved.
- * 
- * @author: LxAnC
- * @date: 2024-04-25 
- */
-//题目有点冗长难以反应,现在来试一下;
-//首先肯定需要定义另一个数组来存储是否被标记;
-//然后依次进行他们的操作;
 class Solution {
 public:
-    vector<long long> unmarkedSumArray(vector<int>& nums, vector<vector<int>>& queries) {
-        int n=nums.size();
-        ranges:: sort(nums);
-        unordered_map<int,bool>mp;
-        vector<int>ans(queries.size());
-        for(auto x:nums)
-            mp[x]=false;
-        for(int i=0;i<queries.size();i++)
-        {
-            for(int j=0;j<2;j++)
-            {
-                if(j==0)
-                if(mp[queries[i][j]]==false)
-                mp[queries[i][j]]=true;
-                if(j==1)
-                {
-                    for(int k=0;k<n;k++)
-                    {
-                        if(mp[nums[k]]==false)
-                        {
-                            mp[nums[k]]==true;
-                            queries[i][j]--;
-                        }
-                    }
+    vector<long long> unmarkedSumArray(vector<int> &nums, vector<vector<int>> &queries) {
+        int n = nums.size();
+        long long s = accumulate(nums.begin(), nums.end(), 0LL);
+        vector<int> ids(n);
+        iota(ids.begin(), ids.end(), 0);
+        ranges::stable_sort(ids, [&](int i, int j) { return nums[i] < nums[j]; });
+
+        vector<long long> ans;
+        int j = 0;
+        for (auto &q : queries) {
+            int i = q[0], k = q[1];
+            s -= nums[i];
+            nums[i] = 0; // 标记
+            for (; j < n && k; j++) {
+                i = ids[j];
+                if (nums[i] > 0) { // 没有被标记
+                    s -= nums[i];
+                    nums[i] = 0;
+                    k--;
                 }
             }
+            ans.push_back(s);
         }
-        cout<<mp[1]<<endl;
+        return ans;
     }
 };
