dffde2ea0b67cdbf8c6def51c58ce69bcc7b06ce
LuXuAn <5001363@qq.com>
2024-04-25 21:45:03 +0800

    第一题还是比较简单

--- VsCode/LC/126week double/3079求和加密数组.cpp
+++ VsCode/LC/126week double/3079求和加密数组.cpp
@@ -5,3 +5,32 @@
  * @date: 2024-04-25 
  */
 //思路就是先找出最大值,然后将改为对应位的数字
+// class Solution {
+// public:
+//     int sumOfEncryptedInt(vector<int>& nums) {
+//         int sum;
+//         int n=nums.size();
+//         for(int i=0;i<n;i++)
+//         {
+//             int elem=nums[i];
+//             int max=nums[i]%10;
+//             int k=0;
+//             while(elem)
+//             {
+//                 if(elem%10>max)
+//                 max=elem%10;
+//                 k++;
+//                 elem/=10;
+//             }
+//             if(k==1)
+//             nums[i]=max;
+//             else if(k==2)
+//             nums[i]=max*10+max;
+//             else if(k==3)
+//             nums[i]=max*100+max*10+max;
+//             else if(k==4)
+//             nums[i]=max*1000+max*100+max*10+max;
+//         }
+//         return accumulate(nums.begin(),nums.end(),0);
+//     }
+// };
