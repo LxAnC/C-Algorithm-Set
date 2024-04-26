517b7944bf63ba4b926bfbd5b905c89907ac8c61
LuXuAn <5001363@qq.com>
2024-04-26 22:13:07 +0800

    优化暴力算法

--- VsCode/LC/389week simple/3084统计以给定字符开头和结尾的子字符串总数.cpp
+++ VsCode/LC/389week simple/3084统计以给定字符开头和结尾的子字符串总数.cpp
@@ -7,26 +7,25 @@
 //思路:
 // 首先肯定先把ret初始化好,然后枚举所有以第二个参数为开头和结尾的
 // 所有子字符串
-class Solution {
-public:
-    long long countSubstrings(string s, char c) {
-        int ret=0;
-        int n=s.length();
-        for(int i=0;i<n;i++)
-        {
-            if(s[i]==c)
-            {
-                int j=i;
-                string ans="";
-                while(j<n)
-                {
-                    ans+=s[j];
-                    if(ans[0]==c&&ans[ans.length()-1]==c)
-                    ret++;
-                    j++;
-                }
-            }
-        }
-        return ret;
-    }
-};
+//第二种思路
+//就是直接找,观察哪种有规律,我这里发现,当两个开头结尾都算就是加2
+//中间有字母就+3,如果后面没有字母就+1
+//第一种超时暴力算法::
+// class Solution {// public://     long long countSubstrings(string s, char c) {//         int ret=0;//         int n=s.length();//         for(int i=0;i<n;i++)
+//         
+//             if(s[i]==c)
+//             {
+//                 int j=i;
+//                 string ans="";
+//                 while(j<n)
+//                 {
+//                     ans+=s[j];
+//                     if(ans[0]==c&&ans[ans.length()-1]==c)
+//                     ret++;
+//                     j++;
+//                 }
+//             }
+//         }
+//         return ret;
+//     }
+// };
