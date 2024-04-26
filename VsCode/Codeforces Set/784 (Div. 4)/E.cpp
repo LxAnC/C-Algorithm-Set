6eabb1174e5eb07dde784fa1a662d9483e2b71bc
LuXuAn <5001363@qq.com>
2024-04-26 22:58:55 +0800

    暴力算法,时间复杂度仍然是On2

--- VsCode/Codeforces Set/784 (Div. 4)/E.cpp
+++ VsCode/Codeforces Set/784 (Div. 4)/E.cpp
@@ -1,15 +1,42 @@
 /**
  * Copyright © 2024 LxAnC. All rights reserved.
- * 
+ *
  * @author: LxAnC
- * @date: 2024-04-26 
+ * @date: 2024-04-26
  */
 #include <bits/stdc++.h>
 
 using namespace std;
 
+void solve()
+{
+  long long n;
+  long long ret=0;
+  cin >> n;
+  vector<string>s(n);
+  for(int i=0;i<n;i++)
+  cin>>s[i];
+  for(int i=0;i<n;i++)
+  {
+      for(int j=i+1;j<n;j++)
+      {
+        if(s[i]==s[j])
+        continue;
+        if(s[i][0]-s[j][0]==0)
+        ret++;
+        else if(s[i][1]-s[j][1]==0)
+        ret++;
+      }
+  }
+    cout<<ret<<endl;
+}
 int main()
 {
-   
-   return 0;
+  int t=1;
+  cin >> t;
+  while (t--)
+  {
+    solve();
+  }
+  return 0;
 }
