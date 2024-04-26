8823fc179ee33fbe2730e52f6d1749997a45aa8e
LuXuAn <5001363@qq.com>
2024-04-26 21:57:46 +0800

    第一次没看数据范围,忽略了长度为1的字符串

--- VsCode/LC/389week simple/3083字符串及其反转是否存在同一子串.cpp
+++ VsCode/LC/389week simple/3083字符串及其反转是否存在同一子串.cpp
@@ -1,1 +1,26 @@
-2
+/**
+ * Copyright © 2024 LxAnC. All rights reserved.
+ * 
+ * @author: LxAnC
+ * @date: 2024-04-26 
+ */
+//还是比较简单,因为题目只限定了长度为2
+//可以直接模拟
+//
+class Solution {
+public:
+    bool isSubstringPresent(string s) {
+        int n=s.length();
+        unordered_map<string,int>d;
+        for(int i=0;i<n-1;i++)
+        {
+            d[s.substr(i,2)]++;
+        }
+        ranges::reverse(s);
+        for(int i=0;i<n-1;i++)
+        if(d.count(s.substr(i,2)))
+        return true;
+        return false;
+
+    }
+};
