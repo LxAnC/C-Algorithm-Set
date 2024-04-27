1cf68e248164f313bfd82608a54b340204e066cc
LuXuAn <5001363@qq.com>
2024-04-27 23:03:02 +0800

    反思一下,已经更新了别人的代码,特别简洁

--- VsCode/Codeforces Set/784 (Div. 4)/F.cpp
+++ VsCode/Codeforces Set/784 (Div. 4)/F.cpp
@@ -7,38 +7,60 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+// t = int(input())
+// for test in range(t):
+//     n = int(input())
+//     a = list(map(int, input().split()))
+//     l = 0
+//     r = n - 1
+//     suml = a[0]
+//     sumr = a[n-1]
+//     ans = 0
+//     while l < r:
+//         if suml == sumr:
+//             ans = max(ans, l + 1 + n - r)
 
-void solve()
-{
-  int n, ret = 0, left, right;
-  int l=0, r=0;
-  cin >> n;
-  vector<int> nums(n);
-  for (int i = 0; i < n; i++)
-  {
-    cin >> nums[i];
-  }
-  left=0,right=n-1;
-  while(left<right)
-  {
-      l+=nums[left];
-      while(right>left&&r<l)
-      {
-        r+=nums[right];
-        right--;
-      }if(r==l)
-      {
-        ret+=n-right-left;
-      }
-  }
-}
-int main()
-{
-  int t;
-  cin >> t;
-  while (t--)
-  {
-    solve();
-  }
-  return 0;
-}
+//         if suml <= sumr:
+//             l+=1
+//             suml+=a[l]
+
+//         elif sumr < suml:
+//             r-=1
+//             sumr+=a[r]
+            
+//     print(ans)
+
+// void solve()
+// {
+//   int n, ret = 0, left, right;
+//   int l=0, r=0;
+//   cin >> n;
+//   vector<int> nums(n);
+//   for (int i = 0; i < n; i++)
+//   {
+//     cin >> nums[i];
+//   }
+//   left=0,right=n-1;
+//   while(left<right)
+//   {
+//       l+=nums[left];
+//       while(right>left&&r<l)
+//       {
+//         r+=nums[right];
+//         right--;
+//       }if(r==l)
+//       {
+//         ret+=n-right-left;
+//       }
+//   }
+// }
+// int main()
+// {
+//   int t;
+//   cin >> t;
+//   while (t--)
+//   {
+//     solve();
+//   }
+//   return 0;
+// }
