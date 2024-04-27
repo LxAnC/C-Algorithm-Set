d64c8df0aab4b0714611ea9846c77f6db8f6b093
LuXuAn <5001363@qq.com>
2024-04-27 22:52:07 +0800

    先想个思路

--- VsCode/Codeforces Set/784 (Div. 4)/F.cpp
+++ VsCode/Codeforces Set/784 (Div. 4)/F.cpp
@@ -8,8 +8,25 @@
 
 using namespace std;
 
+void solve()
+{
+  int n;
+  cin>>n;
+  vector<int>nums(n);
+  for (int  i = 0; i < n; i++)
+  {
+    cin>>nums[i];
+  }
+  
+  
+}
 int main()
 {
-   
+   int t;
+   cin>>t;
+   while(t--)
+   {
+    solve();
+   }
    return 0;
 }
