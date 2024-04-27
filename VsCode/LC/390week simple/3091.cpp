0ac80e2f438f7863278c1c002c0f7d39d9863f5c
LuXuAn <5001363@qq.com>
2024-04-27 20:45:54 +0800

    尝试第二题

---
+++ VsCode/LC/390week simple/3091.cpp
@@ -0,0 +1,16 @@
+/**
+ * Copyright © 2024 LxAnC. All rights reserved.
+ * 
+ * @author: LxAnC
+ * @date: 2024-04-27 
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+//链接如下
+// https://leetcode.cn/problems/apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k/description/
+int main()
+{
+   
+   return 0;
+}
