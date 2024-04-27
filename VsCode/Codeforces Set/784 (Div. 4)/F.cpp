a68dc8375ffdd977e23164e44f651d9cfde9a200
LuXuAn <5001363@qq.com>
2024-04-27 23:01:10 +0800

    操作太繁琐,我有点讨厌我自己的代码了

--- VsCode/Codeforces Set/784 (Div. 4)/F.cpp
+++ VsCode/Codeforces Set/784 (Div. 4)/F.cpp
@@ -1,8 +1,8 @@
 /**
  * Copyright © 2024 LxAnC. All rights reserved.
- * 
+ *
  * @author: LxAnC
- * @date: 2024-04-27 
+ * @date: 2024-04-27
  */
 #include <bits/stdc++.h>
 
@@ -10,23 +10,35 @@ using namespace std;
 
 void solve()
 {
-  int n;
-  cin>>n;
-  vector<int>nums(n);
-  for (int  i = 0; i < n; i++)
+  int n, ret = 0, left, right;
+  int l=0, r=0;
+  cin >> n;
+  vector<int> nums(n);
+  for (int i = 0; i < n; i++)
   {
-    cin>>nums[i];
+    cin >> nums[i];
   }
-  
-  
+  left=0,right=n-1;
+  while(left<right)
+  {
+      l+=nums[left];
+      while(right>left&&r<l)
+      {
+        r+=nums[right];
+        right--;
+      }if(r==l)
+      {
+        ret+=n-right-left;
+      }
+  }
 }
 int main()
 {
-   int t;
-   cin>>t;
-   while(t--)
-   {
+  int t;
+  cin >> t;
+  while (t--)
+  {
     solve();
-   }
-   return 0;
+  }
+  return 0;
 }
