35b89430990878beb4ab0aa2c5bad76de0837b0e
LuXuAn <5001363@qq.com>
2024-04-27 22:04:17 +0800

    听了灵茶山艾府的讲解,弄懂了很多,很多问题结合数学其实是最好的选择,还是要学好数学啊啊啊

--- VsCode/LC/390week simple/3091.cpp
+++ VsCode/LC/390week simple/3091.cpp
@@ -4,9 +4,6 @@
  * @author: LxAnC
  * @date: 2024-04-27 
  */
-#include <bits/stdc++.h>
-
-using namespace std;
 //链接如下
 // https://leetcode.cn/problems/apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k/description/
 class Solution {
