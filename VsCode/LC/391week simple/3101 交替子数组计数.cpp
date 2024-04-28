29dfec7c3f488869f373f5ef0c26a941b74ae077
LuXuAn <5001363@qq.com>
2024-04-28 22:58:55 +0800

    暴力枚举的方法,案例过了

--- VsCode/LC/391week simple/3101 交替子数组计数.cpp
+++ VsCode/LC/391week simple/3101 交替子数组计数.cpp
@@ -10,8 +10,27 @@ using namespace std;
 
 //先讲一下思路
 //就是直接模拟首先写出暴力枚举的写法
-int main()
-{
-   
-   return 0;
-}
+class Solution {
+public:
+    long long countAlternatingSubarrays(vector<int>& nums) {
+        long long ret=0;
+        for(long long i=0;i<nums.size();i++)
+        {
+            long long j=i;
+            long long flag=2;
+            while(j<nums.size())
+            {
+                if(nums[j]!=flag)
+                {flag=nums[j++];
+                ret++;}
+                else 
+                {
+                    break;
+                }
+
+
+            }
+        }
+        return ret;
+    }
+};
