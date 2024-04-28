ffcb9550f9ffd97962a3e18ede5dac1af12523b3
LuXuAn <5001363@qq.com>
2024-04-28 22:40:53 +0800

    先构思

---
+++ VsCode/LC/391week simple/3100换水问题.cpp
@@ -0,0 +1,17 @@
+/**
+ * Copyright © 2024 LxAnC. All rights reserved.
+ * 
+ * @author: LxAnC
+ * @date: 2024-04-28 
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//这是一道模拟题
+首先判断条件就是换的次数没有了,且喝完了所有的满水瓶子
+int main()
+{
+   
+   return 0;
+}
