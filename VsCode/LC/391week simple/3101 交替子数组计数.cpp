f5e3b9d95ac120ec2d323b8ab1632451e2df171c
LuXuAn <5001363@qq.com>
2024-04-28 22:49:15 +0800

    3101交替子数组相邻问题

---
+++ VsCode/LC/391week simple/3101 交替子数组计数.cpp
@@ -0,0 +1,17 @@
+/**
+ * Copyright © 2024 LxAnC. All rights reserved.
+ * 
+ * @author: LxAnC
+ * @date: 2024-04-28 
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//先讲一下思路
+//就是直接模拟首先写出暴力枚举的写法
+int main()
+{
+   
+   return 0;
+}
