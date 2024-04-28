80467508dae84853c0dd2fbeaac6e944b7213ce5
LuXuAn <5001363@qq.com>
2024-04-28 22:15:51 +0800

    刷刷刷

---
+++ VsCode/LC/127week double/3095.cpp
@@ -0,0 +1,19 @@
+/**
+ * Copyright © 2024 LxAnC. All rights reserved.
+ * 
+ * @author: LxAnC
+ * @date: 2024-04-28 
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//链接如下
+//https://leetcode.cn/contest/biweekly-contest-127/
+int main()
+{
+   int n=20;
+   int k=2;
+   cout<<n|0;
+   return 0;
+}
