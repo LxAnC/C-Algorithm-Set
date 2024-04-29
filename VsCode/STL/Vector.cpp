349f641de8c9883e068f7970773b26ba7adf39cf
LuXuAn <5001363@qq.com>
2024-04-29 21:39:34 +0800

    每个方法都有时间复杂度

--- VsCode/STL/Vector.cpp
+++ VsCode/STL/Vector.cpp
@@ -21,21 +21,32 @@ int main()
 
    // 初始化和长度
    vector<int> a(n);
-   vector<int> b(n, 1);//这就是定义了一个长度为n,初始化值为1的vector
+   vector<int> b(n, 1); // 这就是定义了一个长度为n,初始化值为1的vector
 
-   //包含元素的动态数组
-   vector<int>c{1,2,3,4,5};//初始化包含元素的数组
+   // 包含元素的动态数组
+   vector<int> c{1, 2, 3, 4, 5}; // 初始化包含元素的数组
    // for(auto x:c)
    // cout<<x<<endl;
 
-   vector<int>o[5];//定义一个二维数组,第一维固定长度为5第二维可变
+   vector<int> o[5]; // 定义一个二维数组,第一维固定长度为5第二维可变
 
-   vector<vector<int>>nums;//定义一个行和列都可变的数组
-   nums.push_back({1,2,3,4,5});
-   //还可以直接push一个数组
-   //固定长度的初始化二维
-   vector<vector<int>>p(n+1,vector<int>(n+2));//固定格式
-   vector a(n+1,vector(n+1,0));//这是c++17和c++20支持的格式但是不常用
-   
+   vector<vector<int>> nums; // 定义一个行和列都可变的数组
+   nums.push_back({1, 2, 3, 4, 5});
+   // 还可以直接push一个数组
+   // 固定长度的初始化二维
+   vector<vector<int>> p(n + 1, vector<int>(n + 2)); // 固定格式
+   vector a(n + 1, vector(n + 1, 0));                // 这是c++17和c++20支持的格式但是不常用
+
+   // 一些基本的方法函数
+   //  这里以a数组为例
+   a.front();//返回第一个数据O ( 1 ) 
+   a.end();//返回数组中的最后一个数据 O ( 1 ) 
+   a.back();//删除最后一个数据O ( 1 ) 
+   a.pop_back();
+   a.push_back(2);
+   a.size();
+   a.clear();
+   a.resize(2,4);//改变数组的大小为2,里面的元素为4
+
    return 0;
 }
