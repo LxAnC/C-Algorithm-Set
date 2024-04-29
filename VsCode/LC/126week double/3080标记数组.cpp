d4e75989d5218c53d0fde63df6a713921f06605f
LuXuAn <5001363@qq.com>
2024-04-29 22:25:29 +0800

    学习一种新的排序方法

--- VsCode/LC/126week double/3080标记数组.cpp
+++ VsCode/LC/126week double/3080标记数组.cpp
@@ -10,6 +10,14 @@ using namespace std;
 //今天看了视频,决定自己来做带下标排序,冲
 int main()
 {
-   
+   vector<int>nums{1,4,2,6,3,5,4};
+   vector<int>ids;
+
+   int n=nums.size();
+   for(int i=0;i<n;i++)
+   ids[i]=i;
+   sort(ids.begin(),ids.end(),cmp);
+   for(auto x:ids)
+   cout<<x<<endl;
    return 0;
 }
