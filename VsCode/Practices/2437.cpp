c6c404974f5cffcc27e143c2a8d19545d766b2fe
LuXuAn <5001363@qq.com>
2024-04-30 13:43:57 +0800

    一道简单题让我苦了半天模拟了一些时间

---
+++ VsCode/Practices/2437.cpp
@@ -0,0 +1,57 @@
+/**
+ * Copyright © 2024 LxAnC. All rights reserved.
+ *
+ * @author: LxAnC
+ * @date: 2024-04-30
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  string s;
+  s="21:09";
+  int ret = 0;
+  // 小时
+  if (s[0] == '?' || s[1] == '?')
+  {
+    if (s[0] == '?' && s[1] != '?')
+      if (s[1] >= '4')
+        ret += 2;
+      else
+        ret += 3;
+    else if (s[1] == '?' && s[0] != '?')
+      if (s[0] > '1')
+        ret += 4;
+      else
+        ret += 10;
+    else
+      ret = 24;
+  }
+  // 分钟
+  if (s[3] == '?' || s[4] == '?')
+  {
+    if (s[3] == '?' && s[4] != '?')
+      if (ret != 0)
+        ret *= 6;
+      else
+        ret += 6;
+    else if (s[3] != '?' && s[4] == '?')
+    {
+      if (ret != 0)
+        ret *= 10;
+      else
+        ret += 10;
+    }
+    else
+    {
+      if (ret != 0)
+        ret *= 60;
+      else
+        ret += 60;
+    }
+  }
+  cout<<ret == 0 ? 1 : ret;
+  return 0;
+}
