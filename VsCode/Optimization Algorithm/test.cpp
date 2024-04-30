0e37d7f205917ef625c532de6908101fa0b6940b
LuXuAn <5001363@qq.com>
2024-04-30 09:03:13 +0800

    使用Lamda

--- VsCode/Optimization Algorithm/test.cpp
+++ VsCode/Optimization Algorithm/test.cpp
@@ -5,12 +5,14 @@
  * @date: 2024-04-30
  */
 #include <bits/stdc++.h>
-
 using namespace std;
-
+int n = 20;
 int main()
 {
-  cout << __cplusplus << endl;
-  system("pause");
+  // [ capture ]( parameters ) mutable throw() -> return_type { expression }
+  // 捕获与传值传引,控制
+  auto s = [](int a, int b)
+  { return a > b; };
+  auto x=s(1,2);
   return 0;
 }
