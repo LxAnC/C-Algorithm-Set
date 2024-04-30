54a3eb8304d60c69e1a6a317c77b049c1b9bd870
LuXuAn <5001363@qq.com>
2024-04-30 10:48:46 +0800

    新特性手动实现

--- VsCode/Optimization Algorithm/test.cpp
+++ VsCode/Optimization Algorithm/test.cpp
@@ -11,8 +11,20 @@ int main()
 {
   // [ capture ]( parameters ) mutable throw() -> return_type { expression }
   // 捕获与传值传引,控制
-  auto s = [](int a, int b)
-  { return a > b; };
-  auto x=s(1,2);
+  auto e = [](int a, int b)
+  { return a > b; }; // 实现判断函数
+  vector<int> s{1, 3, 2, 5, 3, 42, 4, 0, 6, 3, 2},m;
+  // sort(s.begin(), s.end(), [](int a, int b)
+  //      { return a > b; }); // 最标准的C++20及以下的sort
+  // for(auto &x:s)
+  // {
+  //   if(x%3==0)
+  //   m.push_back(x*x);
+  // }
+  copy_if(s.begin(),s.begin()+4,back_inserter(m),[&](int a){
+return a%3==0;
+  });
+  for (auto &x : m)
+    cout << x << " ";
   return 0;
 }
