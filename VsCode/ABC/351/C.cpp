91efe36dc9fc556619775fda7d2e69593a52af64
LuXuAn <5001363@qq.com>
2024-04-30 22:41:04 +0800

    优化了C

--- VsCode/ABC/351/C.cpp
+++ VsCode/ABC/351/C.cpp
@@ -1,56 +1,30 @@
 /**
  * Copyright © 2024 LxAnC. All rights reserved.
- *
+ * 
  * @author: LxAnC
- * @date: 2024-04-30
+ * @date: 2024-04-30 
  */
 #include <bits/stdc++.h>
 
 using namespace std;
-
-void solve()
-{
-  int n;
-  cin >> n;
-  vector<int> ans;
-  int a[200005];
-  for(int i=0;i<n;i++)
-  {
-  cin>>a[i];
-  }
-  int i=0;
-  while (n)
-  {
-    ans.push_back(pow(2,a[i++]));
-    while(1)
-    if (ans.size() <= 1)
-    {
-      break;
-      
-    }
-    else if (ans[ans.size()-1]!=ans[ans.size()-2])
-    {
-      break;
-      
-    }
-    else
-    {
-      int x = ans[ans.size()-1]+ans[ans.size()-2];
-      ans.erase(ans.begin()+ans.size()-2,ans.begin()+ans.size());
-      ans.push_back(x);
-      
-      continue;
-    }
-    n--;
-  }
-  cout<<ans.size()<<endl;
-}
+int a[100001];
 int main()
 {
-  int t = 1;
-  while (t--)
-  {
-    solve();
-  }
-  return 0;
+   int n;
+   vector<int>nums;
+   cin>>n;
+   for(int i=0;i<n;i++)
+   {
+    int x;
+    cin>>x;
+    while(nums.size()&&nums.back()==x)
+    {
+      x++;
+      nums.pop_back();
+      
+    }
+    nums.push_back(x);
+   }
+   cout<<nums.size();
+   return 0;
 }
