283201bb92498f557ae721a3be1a68afde2c7568
LuXuAn <5001363@qq.com>
2024-04-30 08:48:18 +0800

    更改版本号为C++20以便支持所有的新特性

---
+++ VsCode/Optimization Algorithm/test.cpp
@@ -0,0 +1,16 @@
+/**
+ * Copyright © 2024 LxAnC. All rights reserved.
+ *
+ * @author: LxAnC
+ * @date: 2024-04-30
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  cout << __cplusplus << endl;
+  system("pause");
+  return 0;
+}
