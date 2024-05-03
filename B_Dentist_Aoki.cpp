f01f2335805356f15b9669db482bd2c98f2e1a92
LuXuAn <5001363@qq.com>
2024-05-04 00:14:00 +0800

    老是不细心

--- B_Dentist_Aoki.cpp
+++ B_Dentist_Aoki.cpp
@@ -11,25 +11,22 @@ using namespace std;
 int main()
 {
   int n, q;
-  cin >> n; 
-   int sum=n;
-  vector<int> nums;
-  iota(nums.begin(), nums.end(), 1);
-  unordered_map<int,int>bo;
-  for(int i=0;i<q;i++)
+  cin >> n>>q;
+  int sum = n;
+  int a[10000] = {0};
+  for (int i = 0; i < q; i++)
   {
-      int bit;
-      cin>>bit;
-      if(!(bo.count(bit))||bo[bit]==0)
-      {
-          bo[bit]=1;
-          sum--;
-      }
-      else
-     { bo[bit]=0;
-     sum++;
-     }
+    int x;
+    cin>>x;
+    if(a[x]==0)
+    a[x]=1;
+    else if(a[x]==1)
+    a[x]=0;
+  }
+  for(int i=0;i<10000;i++)
+  {
+    if(a[i]==1)
+    sum--;
   }
   cout<<sum<<endl;
-  return 0;
 }
