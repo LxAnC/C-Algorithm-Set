aa259970cb1a7dd35167af19cb9dbe7f192ef3ac
LuXuAn <5001363@qq.com>
2024-05-03 23:54:26 +0800

    又学习到了一点儿substr取子串的注意事项

--- A_Past_ABCs.cpp
+++ A_Past_ABCs.cpp
@@ -10,6 +10,13 @@ using namespace std;
 
 int main()
 {
-   
+   string s;
+   cin>>s;
+   s=s.substr(3);
+  int  x=stoi(s);
+   if(x<=349&&x!=316)
+   cout<<"Yes"<<endl;
+   else 
+   cout<<"No"<<endl;    
    return 0;
 }
