10cd09472df6c83c15313189d7838dfb37d78316
LuXuAn <5001363@qq.com>
2024-05-07 23:53:59 +0800

    Fix

--- C_Standing_On_The_Shoulders.cpp
+++ C_Standing_On_The_Shoulders.cpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 const long long maxn=2e5+10;
-long long a[maxn],b[maxn];
+long long a[maxn],b[maxn],cnt=0;
 long long c[maxn];
 priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>q;
 int main()
@@ -19,11 +19,14 @@ int main()
    for(long long i=0;i<n;i++)
    {
       cin>>a[i+1]>>b[i+1];
+      
       q.push({i+1,abs(a[i+1]-b[i+1])});
    }
    while(!q.empty())
    {
-      
+      auto g=q.top();
+      cnt+=q.top().first
+
    }
 
    return 0;
