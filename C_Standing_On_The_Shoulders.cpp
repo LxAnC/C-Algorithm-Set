803d3f89f71da967a7ab4ea3cc66d6c24ff43f62
LuXuAn <5001363@qq.com>
2024-05-08 00:07:15 +0800

    奇怪为什么其他的案例都能过

--- C_Standing_On_The_Shoulders.cpp
+++ C_Standing_On_The_Shoulders.cpp
@@ -1,40 +1,40 @@
 /**
  * Copyright © 2024 LxAnC. All rights reserved.
- * 
+ *
  * @author: LxAnC
- * @date: 2024-05-07 
+ * @date: 2024-05-07
  */
 #include <bits/stdc++.h>
 
 using namespace std;
-const long long maxn=2e5+10;
-long long a[maxn],b[maxn],cnt=0,cnth;
-priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>q;
+const long long maxn = 2e5 + 10;
+long long a[maxn], b[maxn], cnt = 0, cnth;
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
 int main()
 {
-   
-   long long n;
-   long long x,y;
-   cin>>n;
-   for(long long i=0;i<n;i++)
-   {
-      cin>>a[i+1]>>b[i+1];
 
-      q.push({i+1,abs(a[i+1]-b[i+1])});
-   }
-   //第一个人
-   auto g=q.top().first;
-   cnt=a[g];
-   cnth=b[g];
-   while(!q.empty())
-   {
-      auto g=q.top();
-      q.pop();cnth=cnt+b[q.top().first];
-      cnt+=a[q.top().first];
-      
-      
-   }
-   cout<<cnth<<endl;
+  long long n;
+  long long x, y;
+  cin >> n;
+  for (long long i = 0; i < n; i++)
+  {
+    cin >> a[i + 1] >> b[i + 1];
 
-   return 0;
+    q.push({i + 1, abs(a[i + 1] - b[i + 1])});
+  }
+  // 第一个人
+  auto g = q.top().first;
+  cnt = a[g];
+  cnth = b[g];
+  q.pop();
+  while (!q.empty())
+  {
+    auto g = q.top().first;
+    cnth = cnt + b[g];
+    cnt += a[g];
+    q.pop();
+  }
+  cout << cnth << endl;
+
+  return 0;
 }
