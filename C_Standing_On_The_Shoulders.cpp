908c453caf904701d7336cbe63f98395c35948b9
LuXuAn <5001363@qq.com>
2024-05-08 00:03:33 +0800

    就差一点儿了

--- C_Standing_On_The_Shoulders.cpp
+++ C_Standing_On_The_Shoulders.cpp
@@ -9,25 +9,29 @@
 using namespace std;
 const long long maxn=2e5+10;
 long long a[maxn],b[maxn],cnt=0,cnth;
-long long c[maxn];
 priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>q;
 int main()
 {
    
    long long n;
    long long x,y;
+   cin>>n;
    for(long long i=0;i<n;i++)
    {
       cin>>a[i+1]>>b[i+1];
 
       q.push({i+1,abs(a[i+1]-b[i+1])});
    }
+   //第一个人
+   auto g=q.top().first;
+   cnt=a[g];
+   cnth=b[g];
    while(!q.empty())
    {
       auto g=q.top();
-      q.pop();
+      q.pop();cnth=cnt+b[q.top().first];
       cnt+=a[q.top().first];
-      cnth=cnt+b[q.top().first];
+      
       
    }
    cout<<cnth<<endl;
