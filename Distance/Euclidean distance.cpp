e5840f3f00bd6887329c3a5ce6c94bcf44fb17f4
LuXuAn <5001363@qq.com>
2024-05-08 15:52:40 +0800

    Comp

--- Distance/Euclidean distance.cpp
+++ Distance/Euclidean distance.cpp
@@ -10,6 +10,9 @@ using namespace std;
 
 int main()
 {
-   
+   int x[2],y[2];
+   cin>>x[0]>>y[0];
+   cin>>x[1]>>y[1];
+   cout<<(sqrt((x[0]-x[1])*(x[0]-x[1])+(y[0]-y[1])*(y[0]-y[1])));
    return 0;
 }
