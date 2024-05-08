5f5b382cbdcfa65e2b235b71c79459b21980398b
LuXuAn <5001363@qq.com>
2024-05-08 18:04:30 +0800

    理解一下把

--- D_Permutation_Subsequence.cpp
+++ D_Permutation_Subsequence.cpp
@@ -9,12 +9,12 @@ int main()
   cin.tie(0);
   cout.tie(0);
   int n, k;
-  cin >> n >> k;//先输入最先的数字,k为序列个数
+  cin >> n >> k; // 先输入最先的数字,k为序列个数
   vector<int> P(n);
   int p;
   for (int i = 0; i < n; i++)
   {
-    cin >> p;//这里是
+    cin >> p; // 这里是
     p--;
     P[p] = i;
   }
@@ -22,16 +22,16 @@ int main()
   set<int> S;
   for (int i = 0; i < k; i++)
   {
-    S.insert(P[i]);//插入他们的位置
+    S.insert(P[i]); // 插入他们的位置
   }
 
-  int ans = *S.rbegin() - *S.begin();//算出最开始的值
+  int ans = *S.rbegin() - *S.begin(); // 算出最开始的值
 
   for (int i = k; i < n; i++)
   {
-    S.erase(P[i - k]);
+    S.erase(P[i - k]); // 使用这两个来更新值
     S.insert(P[i]);
-    ans = min(ans, *S.rbegin() - *S.begin());
+    ans = min(ans, *S.rbegin() - *S.begin()); // 更新最小索引差值
   }
 
   cout << ans << endl;
