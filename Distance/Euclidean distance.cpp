cb702f52681f6819534664d09f97c11738b33c0b
LuXuAn <5001363@qq.com>
2024-05-08 15:48:41 +0800

    欧几里得距离更新

---
+++ Distance/Euclidean distance.cpp
@@ -0,0 +1,15 @@
+/**
+ * Copyright © 2024 LxAnC. All rights reserved.
+ * 
+ * @author: LxAnC
+ * @date: 2024-05-08 
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+   
+   return 0;
+}
