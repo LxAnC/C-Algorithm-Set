c4524041bdfc13ab017169fd3ca123e53218ce99
LuXuAn <5001363@qq.com>
2024-05-08 17:07:43 +0800

    优化开始

--- Algorithm and Anay/Maximum sub segment total.cpp
+++ Algorithm and Anay/Maximum sub segment total.cpp
@@ -11,32 +11,34 @@ int n;
 const int maxn = 1000;
 vector<vector<int>> dp(maxn + 1,vector<int>(maxn+1));
 int a[maxn],ret=INT_MIN;
-int solve()
-{
-  int sum=0;
-  //状态转移方程
-  //最终输出dp[n][n];
-//ret记录当前最大的子区间和 
-  for(int i=0;i<=n;i++)
-  {
-      dp[i][i]=a[i];
-      for(int j=i+1;j<=n;j++)
-      {
-        dp[i][j]=dp[i][j-1]+a[j];
-        ret=max(dp[i][j],ret);
-      }
-  }
-  return ret;
 
-}
-int main()
-{
 
-  cin >> n;
-  for(int i=1;i<=n;i++)
-  {
-    cin>>a[i];
-  }
-  cout<<solve();
-  return 0;
-}
+// int solve()
+// {
+//   int sum=0;
+//   //状态转移方程
+//   //最终输出dp[n][n];
+// //ret记录当前最大的子区间和 
+//   for(int i=0;i<=n;i++)
+//   {
+//       dp[i][i]=a[i];
+//       for(int j=i+1;j<=n;j++)
+//       {
+//         dp[i][j]=dp[i][j-1]+a[j];
+//         ret=max(dp[i][j],ret);
+//       }
+//   }
+//   return ret;
+
+// }
+// int main()
+// {
+
+//   cin >> n;
+//   for(int i=1;i<=n;i++)
+//   {
+//     cin>>a[i];
+//   }
+//   cout<<solve();
+//   return 0;
+// }
