212d487990f78e32d0ed3158f18e782f02183de2
LuXuAn <5001363@qq.com>
2024-05-08 17:23:44 +0800

    自己做的太垃圾了,贴一个大众做法

--- C_Standing_On_The_Shoulders.cpp
+++ C_Standing_On_The_Shoulders.cpp
@@ -4,37 +4,53 @@
  * @author: LxAnC
  * @date: 2024-05-07
  */
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
-const long long maxn = 2e5 + 10;
-long long a[maxn], b[maxn], cnt = 0, cnth;
-priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
-int main()
-{
-
-  long long n;
-  long long x, y;
-  cin >> n;
-  for (long long i = 0; i < n; i++)
-  {
-    cin >> a[i + 1] >> b[i + 1];
-
-    q.push({i + 1, abs(a[i + 1] - b[i + 1])});
-  }
-  // 第一个人
-  auto g = q.top().first;
-  cnt = a[g];
-  cnth = b[g];
-  q.pop();
-  while (!q.empty())
-  {
-    auto g = q.top().first;
-    cnth = cnt + b[g];
-    cnt += a[g];
-    q.pop();
-  }
-  cout << cnth << endl;
-
-  return 0;
+long long a[200005],b[200005];
+int main(){
+	int n;
+	cin>>n; 
+	long long sum=0;
+	long long maxx=0;
+	for(int i=1;i<=n;i++){
+		cin>>a[i]>>b[i];
+		sum+=a[i];//这是所有巨人身体的高度相加 
+		maxx=max(maxx,b[i]-a[i]);//这是最大的巨人头 
+	}
+	cout<<maxx+sum;//相加即为答案 
+	return 0; 
 }
+// #include <bits/stdc++.h>
+
+// using namespace std;
+// const long long maxn = 2e5 + 10;
+// long long a[maxn], b[maxn], cnt = 0, cnth;
+// priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
+// int main()
+// {
+
+//   long long n;
+//   long long x, y;
+//   cin >> n;
+//   for (long long i = 1; i <= n; i++)
+//   {
+//     cin >> a[i] >> b[i];
+
+//     q.push({i, abs(a[i] - b[i])});
+//   }
+//   // 第一个人
+//   auto g = q.top().first;
+//   cnt = a[g];
+//   cnth = b[g];
+//   q.pop();
+//   while (!q.empty())
+//   {
+//     auto g = q.top().first;
+//     cnth = cnt + b[g];
+//     cnt += a[g];
+//     q.pop();
+//   }
+//   cout << cnth << endl;
+
+//   return 0;
+// }
