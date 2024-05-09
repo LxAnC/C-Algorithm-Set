d9ec29a2e88ebca4b9ccf0ad3e52e4d37eb1ff56
LuXuAn <5001363@qq.com>
2024-05-09 17:44:22 +0800

    从新做一下

--- D_New_Friends.cpp
+++ D_New_Friends.cpp
@@ -1,68 +1,81 @@
 // LUOGU_RID: 156571137
 // Problem: D - New Friends
-// LuoguUID: 514700 
+// LuoguUID: 514700
 // LuoguUserName: MicroSun
-// 
+//
 // Powered by CP Editor (https://cpeditor.org)
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define endl '\n'
 #define pb push_back
 #define fst first
 #define scd second
-#define rep(i,s,e) for(int i=s;i<=e;i++)
-#define dep(i,s,e) for(int i=s;i>=e;i--)
+#define rep(i, s, e) for (int i = s; i <= e; i++)
+#define dep(i, s, e) for (int i = s; i >= e; i--)
 
-using ll=long long;
-using pii=pair<int,int>;
-using pll=pair<ll,ll>;
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
 
-const int maxn=2e5+10;
+const int maxn = 2e5 + 10;
 
-struct edge{
-	int to,nxt;
-}a[maxn<<1];
-int head[maxn],cnt;
-void add(int u,int v){
-	a[++cnt]={v,head[u]};
-	head[u]=cnt;
+struct edge
+{
+	int to, nxt;
+} a[maxn << 1];
+int head[maxn], cnt;
+void add(int u, int v)
+{
+	a[++cnt] = {v, head[u]};
+	head[u] = cnt;
 }
 ll cnt1;
 bool vis[maxn];
-void dfs(int u,int f){
-	if(vis[u]) return;
-	vis[u]=1;
+void dfs(int u, int f)
+{
+	if (vis[u])
+		return;
+	vis[u] = 1;
 	++cnt1;
-	for(int i=head[u];i;i=a[i].nxt){
-		int t=a[i].to;
-		if(t!=f) dfs(t,u);
+	for (int i = head[u]; i; i = a[i].nxt)
+	{
+		int t = a[i].to;
+		if (t != f)
+			dfs(t, u);
 	}
 }
-void solve(){
-	int n,m;
-	cin>>n>>m;
-	rep(i,1,m){
-		int a,b;
-		cin>>a>>b;
-		add(a,b);add(b,a);
+void solve()
+{
+	int n, m;
+	cin >> n >> m;
+	rep(i, 1, m)
+	{
+		int a, b;
+		cin >> a >> b;
+		add(a, b);
+		add(b, a);
 	}
-	ll ans=0;
-	rep(i,1,n){
-		if(!vis[i]){
-			dfs(i,i);
-			ans+=cnt1/2.0*(cnt1-1);
-			cnt1=0;
+	ll ans = 0;
+	rep(i, 1, n)
+	{
+		if (!vis[i])
+		{
+			dfs(i, i);
+			ans += cnt1 / 2.0 * (cnt1 - 1);
+			cnt1 = 0;
 		}
 	}
-	cout<<ans-m;
+	cout << ans - m;
 }
-int main(){
+int main()
+{
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
-	int t=1;
-	//cin>>t;
-	while(t--) solve();
-    return 0;
+	int t = 1;
+	// cin>>t;
+	while (t--)
+		solve();
+	return 0;
 }
--- D_Permutation_Subsequence.cpp
+++ D_Permutation_Subsequence.cpp
@@ -10,30 +10,5 @@ int main()
   cout.tie(0);
   int n, k;
   cin >> n >> k; // 先输入最先的数字,k为序列个数
-  vector<int> P(n);
-  int p;
-  for (int i = 0; i < n; i++)
-  {
-    cin >> p; // 这里是
-    p--;
-    P[p] = i;
-  }
-
-  set<int> S;
-  for (int i = 0; i < k; i++)
-  {
-    S.insert(P[i]); // 插入他们的位置
-  }
-
-  int ans = *S.rbegin() - *S.begin(); // 算出最开始的值
-
-  for (int i = k; i < n; i++)
-  {
-    S.erase(P[i - k]); // 使用这两个来更新值
-    S.insert(P[i]);
-    ans = min(ans, *S.rbegin() - *S.begin()); // 更新最小索引差值
-  }
-
-  cout << ans << endl;
-  return 0;
+  
 }
