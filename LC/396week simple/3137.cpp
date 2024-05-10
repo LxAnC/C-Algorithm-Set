084a08224d1706a61b9e6e5e971be865a4cd702d
LuXuAn <5001363@qq.com>
2024-05-10 22:28:11 +0800

    第一次直接过了

--- LC/396week simple/3137.cpp
+++ LC/396week simple/3137.cpp
@@ -4,12 +4,28 @@
  * @author: LxAnC
  * @date: 2024-05-10 
  */
-#include <bits/stdc++.h>
-
-using namespace std;
-
-int main()
-{
-   
-   return 0;
-}
+class Solution {
+public:
+    int minimumOperationsToMakeKPeriodic(string word, int k) {
+            unordered_map<string,int>hash;
+            int n=word.size();
+            string d;
+            int maxa=INT_MIN;
+            int ret=0;
+            for(int i=0;i<n;i+=k)
+            {
+                string s=word.substr(i,k);
+                hash[s]++;
+                if(maxa<hash[s])
+                d=s,maxa=hash[s];
+            }
+            for(int i=0;i<n;i+=k)
+            {
+                string b=word.substr(i,k);
+                if(b!=d)
+                ret++;
+            }
+            return  ret;
+            
+    }
+};
