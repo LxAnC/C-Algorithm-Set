1c321aa37de6e6caca34738c2e73c680ec3fc2bf
LuXuAn <5001363@qq.com>
2024-05-10 21:52:26 +0800

    第一题就是有些判断还是很好写的

--- LC/396week simple/3136.cpp
+++ LC/396week simple/3136.cpp
@@ -7,9 +7,30 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+// 有效单词 需要满足以下几个条件：
 
-int main()
-{
-   
-   return 0;
-}
+// 至少 包含 3 个字符。
+// 由数字 0-9 和英文大小写字母组成。（不必包含所有这类字符。）
+// 至少 包含一个 元音字母 。
+// 至少 包含一个 辅音字母 。
+// 给你一个字符串 word 。如果 word 是一个有效单词，则返回 true ，否则返回 false 。
+
+// 注意：
+
+// 'a'、'e'、'i'、'o'、'u' 及其大写形式都属于 元音字母 。
+// class Solution {
+// public:
+//     bool isValid(string word) {
+//         bool ret=false;
+//         int f1=0,f2=0;
+//         return ret;for(int i=0;i<word.size();i++)
+//         word[i]=tolower(word[i]);
+//         for(auto x:word){
+//             if(x=='a'||x=='e'||x=='i'||x=='o'||x=='u')
+//             f1=1;
+//             if(!(x=='a'||x=='e'||x=='i'||x=='o'||x=='u')&&x>='a'&&x<='z')
+//             f2=1;
+//         }
+//         return f1==1&&f2==1?true:false;
+//     }
+// };
