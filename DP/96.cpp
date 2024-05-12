7251a45739edfdffb89571688dad8de5453da099
LuXuAn <5001363@qq.com>
2024-05-12 21:34:24 +0800

    Update

--- DP/96.cpp
+++ DP/96.cpp
@@ -17,8 +17,3 @@ public:
         return dp[n];
     }
 };
-
-作者：代码随想录
-链接：https://leetcode.cn/problems/unique-binary-search-trees/solutions/550154/96-bu-tong-de-er-cha-sou-suo-shu-dong-ta-vn6x/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
