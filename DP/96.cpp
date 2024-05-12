794e0e5e78dc437d01bbddd89249b17a870ee229
LuXuAn <5001363@qq.com>
2024-05-12 21:22:58 +0800

    96不同的二叉搜索树题解

--- DP/96.cpp
+++ DP/96.cpp
@@ -4,12 +4,21 @@
  * @author: LxAnC
  * @date: 2024-05-12 
  */
-#include <bits/stdc++.h>
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> dp(n + 1);
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        return dp[n];
+    }
+};
 
-using namespace std;
-
-int main()
-{
-   
-   return 0;
-}
+作者：代码随想录
+链接：https://leetcode.cn/problems/unique-binary-search-trees/solutions/550154/96-bu-tong-de-er-cha-sou-suo-shu-dong-ta-vn6x/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
