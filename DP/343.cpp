bc1fb9631cddafe6f6d8da2278e417f4d804cb8c
LuXuAn <5001363@qq.com>
2024-05-12 21:20:29 +0800

    343整数拆分题解(灵茶山)

--- DP/343.cpp
+++ DP/343.cpp
@@ -4,12 +4,17 @@
  * @author: LxAnC
  * @date: 2024-05-12 
  */
-#include <bits/stdc++.h>
-
-using namespace std;
-
-int main()
-{
-   
-   return 0;
-}
+class Solution {
+public:
+    int integerBreak(int n) {
+        vector <int> dp(n + 1);
+        for (int i = 2; i <= n; i++) {
+            int curMax = 0;
+            for (int j = 1; j < i; j++) {
+                curMax = max(curMax, max(j * (i - j), j * dp[i - j]));
+            }
+            dp[i] = curMax;
+        }
+        return dp[n];
+    }
+};
