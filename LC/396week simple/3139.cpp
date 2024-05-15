e8b2ef54e278e186d02f40d6ed0ce723d83da6ce
LxAnC <5001363@qq.com>
2024-05-15 14:46:34 +0800

    修改了一下

--- LC/396week simple/3139.cpp
+++ LC/396week simple/3139.cpp
@@ -47,8 +47,3 @@ public:
         return ans % MOD;
     }
 };
-
-作者：TsReaper
-链接：https://leetcode.cn/problems/minimum-cost-to-equalize-array/solutions/2765974/mei-ju-by-tsreaper-l8id/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
