bd8a980ccf185d76287a443ee1b613dedf048742
LxAnC <5001363@qq.com>
2024-05-15 16:54:21 +0800

    迪杰斯特拉算法的更新

---
+++ Algorithm and Anay/dijstra.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //各种关系的图论
+    int graph [][4]= {
+    {0, 2, -1, 6},
+    {2, 0, 3, 2 },
+    {-1, 3, 0, 2},
+    {6, 2, 2, 0 }
+    };
+    for(int i=0;i<4;i++)
+    {
+        for(int j=0;j<4;j++)
+        {
+            
+        }
+    }
+}
