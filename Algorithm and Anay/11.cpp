c4685f34a684472cc077af88b462010e6ee465eb
LxAnC <5001363@qq.com>
2024-05-16 23:32:15 +0800

    版本正确，查看一下

--- Algorithm and Anay/11.cpp
+++ Algorithm and Anay/11.cpp
@@ -5,9 +5,8 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    int s=
     //|view::aoti(1,10);
     // vector<int>s;
     // ranges::sort(s);
-    cout<<__cplusplus<<endl
+    cout<<__cplusplus<<endl;
 }
