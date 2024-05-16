35419ecfc2c4f5ab616ca4a9c935fd00b153b38c
LxAnC <5001363@qq.com>
2024-05-16 23:37:03 +0800

    20240517留

--- Algorithm and Anay/11.cpp
+++ Algorithm and Anay/11.cpp
@@ -5,11 +5,11 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    //|view::aoti(1,10);
     vector<int>s{1,2,3,4,5,8,7,6,4,2};
 
     ranges::sort(s);
     for(auto x:s)
     cout<<x<<endl;//输出结果正确
     cout<<__cplusplus<<endl;
+    //20240517留
 }
