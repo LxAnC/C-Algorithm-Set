41d340ec1be81e77783b488b212a715027889e00
LxAnC <5001363@qq.com>
2024-05-17 10:27:07 +0800

    解决了代码错误

--- Algorithm and Anay/11.cpp
+++ Algorithm and Anay/11.cpp
@@ -1,15 +1,14 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    vector<int>s{1,2,3,4,5,8,7,6,4,2};
-//大佬说要设置提示的标准
+    vector<int> s{1, 2, 3, 4, 5, 8, 7, 6, 4, 2};
+    // 大佬说要设置提示的标准
     ranges::sort(s);
-    for(auto x:s)
-    cout<<x<<endl;//输出结果正确
-    cout<<__cplusplus<<endl;
-    //20240517留
+    for (auto x : s)
+        cout << x << endl; // 输出结果正确
+    cout << __cplusplus << endl;
 }
