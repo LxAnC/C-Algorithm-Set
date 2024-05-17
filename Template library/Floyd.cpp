8b3ca9706064a9b6641df7ed43f2d812794c43ca
LxAnC <5001363@qq.com>
2024-05-17 15:30:38 +0800

    大致

--- Template library/Floyd.cpp
+++ Template library/Floyd.cpp
@@ -3,16 +3,18 @@ using namespace std;
 int ret=0;
 int n,m;
 const int maxn=1e5+10;
-int graph[maxn][maxn];
+int graph[maxn][maxn];//图论的数组
 //第一，创建一个距离数组
-int distince[][];
+int dis[maxn][maxn];//当前点到其他点的最短路径
 void floyd()
 {
     for(int j=1;j<=n;j++)
     {
         for(int i=1;i<=n;i++)
         {
-            if(graph[i][j]!=INFINITY)
+            if(graph[i][j]!=INT_MAX)
+            for(int k=1;k<=n;k++)
+              if()
         }
     }
 }
@@ -22,7 +24,14 @@ int main()
     for(int i=1;i<=n;i++)
       for(int j=1;j<=n;j++)
         graph[i][j]=INT_MAX;
-
+    
+    for(int i=1;i<=m;i++)
+    {
+        int a,b,w;
+        cin>>a>>b;
+        cin>>w;
+        graph[a][b]=graph[b][a]=w;
+    }
     floyd();
     cout<<ret<<endl;
 }
