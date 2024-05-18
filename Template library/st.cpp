f69c51eb52442b1169b2a950fd5f9895d2c933ff
LxAnC <5001363@qq.com>
2024-05-18 22:09:57 +0800

    ST表的基本实现

--- Template library/st.cpp
+++ Template library/st.cpp
@@ -4,7 +4,7 @@ using namespace std;
 // 可以最小的时间复杂度来实现某一个区间的最大值
 // st
 int n, m;
-int x;
+int x, y;
 const int maxn = 1005;
 int f[maxn][maxn];
 long long int llg[maxn];
@@ -16,22 +16,22 @@ void init()
         f[i][0] = x;
     }
 
-
-    llg[0]=-1;
+    llg[0] = -1;
     for (int i = 1; i <= maxn; i++)
     {
         llg[i] = llg[i >> 1] + 1;
     }
 
-    //这一段就找到了那个指数
-    for (int i = 1; i <= maxn; i++)
-        cout << llg[i] << " ";
+    // 这一段就找到了那个指数
+    //  for (int i = 1; i <= maxn; i++)
+    //      cout << llg[i] << " ";
 }
 
-// int st()
-// {
-//     for()
-// }
+int st(int l, int r)
+{
+    int k=llg[r-l+1];
+    return max(f[l][k],f[l+(1>>k)][k]);
+}
 // 定义一个函数用于返回查询的值
 int main()
 {
@@ -41,6 +41,13 @@ int main()
     {
         for (int j = 1; j + (1 << (i - 1)) <= n; j++)
             f[j][i] = max(f[j][i - 1], f[j + (1 << (i - 1))][i - 1]);
+    }
+    for (int i = 1; i <= m; i++)
+    {
+
+        cin >> x >> y;
+        cout << st(x, y) << endl;
+        ;
     }
     return 0;
 }
