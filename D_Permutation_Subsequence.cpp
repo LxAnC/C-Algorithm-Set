765e67e836fe9a09d8eeef2ef657414f08dbf697
LxAnC <5001363@qq.com>
2024-05-18 12:32:32 +0800

    题目大意找差值最大的连续元素的下表之差值

--- D_Permutation_Subsequence.cpp
+++ D_Permutation_Subsequence.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
 #include <vector>
 #include <set>
+#include <queue>
 using namespace std;
+const int maxn=2e5+5;
+int nums[maxn];
+int p[maxn];
+int n,k;
 
 int main()
 {
+
   ios::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
-  int n, k;
-  cin >> n >> k; // 先输入最先的数字,k为序列个数
-  
+  int left,right;
+  cin>>n>>k;
+  for(int i=1;i<=n;i++)
+  {
+    cin>>nums[i];
+    p[nums[i]]=i;
+  }
+  for(int i=1;i+k-1<=n;i++)
+  {
+    ret=min(ret,p[nums[i]]-p[nums[i+k-1]]);
+  }
+  while(1)
 }
