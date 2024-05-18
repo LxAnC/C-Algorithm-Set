ebcaf55899588a8dfcaea16c125ea9aa65d038ed
LxAnC <5001363@qq.com>
2024-05-18 20:52:30 +0800

    学习ST表的查询

---
+++ Template library/st.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+//本程序用于学习st表的查询
+//可以最小的时间复杂度来实现某一个区间的最大值
+//st
+ void init()
+ {
+    int 
+ }
+
+
+
+
+int st()
+{
+    for()
+}
+//定义一个函数用于返回查询的值
+int main()
+{
+    
+}
