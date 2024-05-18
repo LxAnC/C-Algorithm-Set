0b1e434cec31dcd74f7780f29c1c1773fd790e9a
LxAnC <5001363@qq.com>
2024-05-18 22:47:03 +0800

    Fix

--- Template library/st.cpp
+++ Template library/st.cpp
@@ -29,7 +29,7 @@ void init()
 int st(int l, int r)
 {
     int k = llg[r - l + 1];
-    return max(f[l][k], f[r - (1 >> k)][k]);
+    return max(f[l][k], f[r - (1 << k)+1][k]);
 }
 // 定义一个函数用于返回查询的值
 int main()
@@ -48,26 +48,4 @@ int main()
         cout << st(x, y) << endl;
     }
     return 0;
-}
-#include <iostream>
-using  namespace std;
-
-int n,m,x,y,a[100010],lg[100010],f[100010][20];
-
-int main()
-{
-	cin>>n>>m; lg[1]=0;
-	for (int i=2;i<=n;i++) lg[i]=lg[i>>1]+1;
-	for (int i=1;i<=n;i++) cin>>f[i][0];
-	for (int j=1;j<=lg[n];j++)
-	for (int i=1;i<=n-(1<<j)+1;i++){
-		f[i][j]=max(f[i][j-1],f[i+(1<<(j-1))][j-1]);
-	}
-	
-	
-	for (int i=1;i<=m;i++)
-	{
-		cin>>x>>y; int l=lg[y-x+1];
-		cout<<max(f[x][l],f[y-(1<<l)+1][l])<<endl;
-	}
 }
