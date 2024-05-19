e2803af3ee6d7761acffc3ed5522dc1890d84630
LxAnC <5001363@qq.com>
2024-05-19 23:26:38 +0800

    提交一下

--- C.cpp
+++ C.cpp
@@ -2,12 +2,16 @@
 using namespace std;
 int main()
 {
-    int i,j,k;
+    int ret=0;
     string x;
     cin>>x;
     int n=x.length();
     for(int i=0;i<n;i++)
     {
-        
+        if(x[i]>='a'&&x[i]<='z')
+        ret++;
     }
+    
+    cout<<ret;
+    return 0;
 }
