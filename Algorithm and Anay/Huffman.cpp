39f3e92f794f46a2af6bfd94500590559a22a7fe
LxAnC <5001363@qq.com>
2024-05-20 11:01:07 +0800

    慢慢做

--- Algorithm and Anay/Huffman.cpp
+++ Algorithm and Anay/Huffman.cpp
@@ -2,9 +2,16 @@
 using namespace std;
 int N=10;//节点数
 const int maxn=1e5+10;
-typedef struct huffmanTree {
+typedef struct  {
     int weight;
     int lch,rch,parent;
     int data;
 }hNode,*huffmanTree;
-
+typedef  char** huffmanCode;
+int inithuffmanTree(huffmanTree &ht);//h
+void createHuffmanTree(huffmanTree &ht,int n);
+void createHuffmanCode(huffmanTree &ht,huffmanCode &hc,int n)
+int main()
+{
+    
+}
