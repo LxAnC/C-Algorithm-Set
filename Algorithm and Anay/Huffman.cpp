9d2278dd4b87e2ae78f2a746ad8cedd60860db62
LxAnC <5001363@qq.com>
2024-05-20 11:58:24 +0800

    写完了

--- Algorithm and Anay/Huffman.cpp
+++ Algorithm and Anay/Huffman.cpp
@@ -8,22 +8,27 @@ typedef struct
     int lch, rch, parent;
     char data;
 } hNode, *huffmanTree;
-typedef char **huffmanCode;
+typedef char** huffmanCode;
 int inithuffmanTree(huffmanTree &ht); // h
-void createHuffmanTree(huffmanTree &ht, int n);
-void createHuffmanCode(huffmanTree &ht, huffmanCode &hc, int n);
+void createhuffmanTree(huffmanTree &ht, int n);
+void createhuffmanCode(huffmanTree &ht, huffmanCode &hc, int n);
 int main()
 {
     huffmanTree ht;
     inithuffmanTree(ht);
     huffmanCode hc;
-    createHuffmanTree(ht, N);
-    createHuffmanCode(ht, hc, N);
+    createhuffmanTree(ht, N);
+    createhuffmanCode(ht, hc, N);
     for (int i = 1; i <= N; i++)
     {
         cout << ht[i].data << '\t';
         cout << hc[i];
     }
+    for (int i = 1; i <= N; i++)								//遍历输出编码
+	{
+		printf("%c:\t",ht[i].data);
+		printf("%s\n", hc[i]);
+	}
     return 0;
 }
 int inithuffmanTree(huffmanTree &ht)
@@ -34,50 +39,67 @@ int inithuffmanTree(huffmanTree &ht)
     }
     for (int i = 1; i <= N; i++)
     {
-        cout<<"请输入节点"<<endl;
-        cin>>ht[i].data;
-        cout<<"请输入每个权值"<<endl;
-        cin>>ht[i].weight;
+        cout << "请输入节点" << endl;
+        cin >> ht[i].data;
+        cout << "请输入每个权值" << endl;
+        cin >> ht[i].weight;
     }
     return 0;
 }
-//队列优化
-void createhuffmanTree(huffmanTree &ht,int n)
+// 队列优化
+void createhuffmanTree(huffmanTree &ht, int n)
 {
-    int min1,min2;
-    int rpos,lpos;
-    for(int i=n+1;i<=2*n-1;i++)
+    if(n<=1)
+    return ;
+    int min1, min2;
+    int rpos, lpos;
+    for (int i = n + 1; i <= 2 * n - 1; i++)
     {
-        min1=INT_MAX;
-        min2=INT_MAX;
-        for(int j=1;j<=n;j++)
+        min1 = INT_MAX;
+        min2 = INT_MAX;
+        for (int j = 1; j <= i-1; j++)
         {
-            if(ht[j].weight<min1&&ht[j].parent==-1)
-              {
-                min2=min1;
-                rpos=lpos;
-                min1=ht[j].weight;
-                lpos=j;
-              }
-              else if(ht[j].weight<min2&&ht[i].parent==-1)
-              {
-                min2=ht[j].weight;
-                rpos=j;
-              }
+            if (ht[j].weight < min1 && ht[j].parent == -1)
+            {
+                min2 = min1;
+                rpos = lpos;
+                min1 = ht[j].weight;
+                lpos = j;
+            }
+            else if (ht[j].weight < min2 && ht[i].parent == -1)
+            {
+                min2 = ht[j].weight;
+                rpos = j;
+            }
         }
-        ht[i].weight=min1+min2;
-        ht[i].rch=rpos;
-        ht[i].lch=lpos;
-        ht[rpos].parent=ht[lpos].parent=i;
+        ht[i].weight = min1 + min2;
+        ht[i].rch = rpos;
+        ht[i].lch = lpos;
+        ht[rpos].parent = ht[lpos].parent = i;
     }
 }
-void createhuffmanCode(huffmanTree &ht,huffmanCode &hc,int n)
+void createhuffmanCode(huffmanTree ht, huffmanCode &hc, int n)
 {
-    int start=0,c=0,f=0;
-    char *code=new char[n];
-    for(int i=1;i<=n;i++)
+    int start = 0, c = 0, f = 0;
+    char *code = new char[n];
+    for (int i = 1; i <= n; i++)
     {
-        
+        start = n - 1;
+        c = i;
+        f = ht[c].parent;
+        while (f != -1)
+        {
+            start--;
+            if (ht[f].lch == c)
+                code[start] = '0';
+            else
+                code[start] = '1';
+            // 依次逼近根
+            c = f;
+            f = ht[c].parent;
+        }
+        // 更新
+        strcpy(hc[i], &code[start]);
     }
-
+    delete[] code;
 }
