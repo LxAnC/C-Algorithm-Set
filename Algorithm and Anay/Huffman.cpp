02727e62cb1dd1a7bf0aaab4f8db175e63240506
LxAnC <5001363@qq.com>
2024-05-20 10:55:16 +0800

    哈夫曼编码的构造

--- Algorithm and Anay/Huffman.cpp
+++ Algorithm and Anay/Huffman.cpp
@@ -1,7 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
-{
-    int i;
-    
-}
+int N=10;//节点数
+const int maxn=1e5+10;
+typedef struct huffmanTree {
+    int weight;
+    int lch,rch,parent;
+    int data;
+}hNode,*huffmanTree;
+
