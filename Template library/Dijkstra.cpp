e3a5e33fd7f7e9bdebfc15e2e34f6956c52df025
LxAnC <5001363@qq.com>
2024-05-21 10:57:39 +0800

    完整弗洛伊德算法求每个点的最短路径

---
+++ Template library/Dijkstra.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+const int maxn=1e5+10;
+
+signed main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int graph[][]={
+        
+    }
+}
--- Template library/Floyd.cpp
+++ Template library/Floyd.cpp
@@ -1,43 +1,40 @@
-#include<iostream>
-#include<climits>
+#include <iostream>
+#include <climits>
 using namespace std;
-int ret=0;
-int n,m;
-const int maxn=1e5+10;
-int graph[maxn][maxn];//图论的数组
+int ret = 0;
+int n=6;
+const int maxn = 1e3 + 10;
+const int max1 = 66666;
+int graph[maxn][maxn] = {
+    {0, 2, 3, 6, max1, max1},
+    {2, 0, max1, max1, 4, 6},
+    {3, max1, 0, 2, max1, max1},
+    {6, max1, 2, 0, 1, 3},
+    {max1, 4, max1, 1, 0, max1},
+    {max1, 6, max1, 3, max1, 0}};
+; // 图论的数组
 void floyd()
 {
-    for(int j=1;j<=n;j++)
+    for (int j = 1; j <= n; j++)
     {
-        for(int i=1;i<=n;i++)
+        for (int i = 1; i <= n; i++)
         {
-            if(graph[i][j]!=INT_MAX)
-              for(int k=1;k<=n;k++)
-                if(graph[i][j]+graph[j][k]<graph[i][k])
-                   graph[i][k]=graph[i][j]+graph[j][k];
+            if (graph[i-1][j-1] != INT_MAX)
+                for (int k = 1; k <= n; k++) // 中间节点
+                    if (graph[i-1][j-1] + graph[j-1][k-1] < graph[i-1][k-1])
+                        graph[i-1][k-1] = graph[i-1][j-1] + graph[j-1][k-1];
         }
     }
     return;
 }
 int main()
 {
-    cin>>n>>m;
-    for(int i=1;i<=n;i++)
-      for(int j=1;j<=n;j++)
-        graph[i][j]=INT_MAX;
-    
-    for(int i=1;i<=m;i++)
-    {
-        int a,b,w;
-        cin>>a>>b;
-        cin>>w;
-        graph[a][b]=graph[b][a]=w;
-    }
     floyd();
-    for(int i=1;i<=n;i++)
-     { for(int j=1;j<=n;j++)
-        cout<<graph[i][j]<<" ";
-        cout<<endl;
-     }
-     return 0;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+            cout << graph[i-1][j-1] << " ";
+        cout << endl;
+    }
+    return 0;
 }
