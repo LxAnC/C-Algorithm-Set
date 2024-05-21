8bd0f7a95a8fdc6bf4ada73c3412661a63ce534e
LxAnC <5001363@qq.com>
2024-05-21 11:31:05 +0800

    Dijkstra邻接矩阵做法更新

--- Template library/Dijkstra.cpp
+++ Template library/Dijkstra.cpp
@@ -2,31 +2,47 @@
 #define int long long
 using namespace std;
 const int maxn = 1e3 + 10;
-int n = 4;
+int n,m;
 int dist[maxn];
 int g[maxn][maxn];
-int st[maxn];
+bool st[maxn];
 
 void dijkstra()
 {
+    memset(dist,0x3f,sizeof dist);
     dist[1]=0;
-    int t=1;
-    while(dist[t])
+    int elem=n;
+    while(elem--)
+    {
+        int t=-1;
+        for(int i=1;i<=n;i++)
+        {
+            if(st[i]==0&&(t==-1||dist[t]>dist[i]))
+            t=i;
+        }
+        st[t]=true;
+        for(int i=1;i<=n;i++)
+        {
+            if(st[i]==0)
+            dist[i]=min(dist[t]+g[t][i],dist[i]);
+        }
+    }
 }
 signed main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    memset(g,0x3f,sizeof,g);
-    for(int i=1;i<=n;i++)
+    cin>>n>>m;
+    memset(g,0x3f,sizeof g);
+    for(int i=1;i<=m;i++)
     {
         int x,y,z;
         cin>>x>>y>>z;
         g[x][y]=min(g[x][y],z);
     }
     dijkstra();
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
         cout << dist[i];
         cout << endl;
