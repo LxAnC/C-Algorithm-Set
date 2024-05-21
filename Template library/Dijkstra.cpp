9c130ff53956a0800985404ac75e5b7c98e006dc
LxAnC <5001363@qq.com>
2024-05-21 11:03:08 +0800

    开搞

--- Template library/Dijkstra.cpp
+++ Template library/Dijkstra.cpp
@@ -7,7 +7,10 @@ signed main()
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    int graph[][]={
-        
-    }
+    int graph[][4]={
+        0,2,-1,6,
+        2,0,3,2,
+        -1,3,0,2,
+        6,2,2,0};//初始化图
+
 }
