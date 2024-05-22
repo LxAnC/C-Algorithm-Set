d826d555e3f97e54996bd309746f98d05f43012a
LxAnC <5001363@qq.com>
2024-05-22 17:12:26 +0800

    贪心算法求背包

---
+++ Algorithm_and_Anay/bags.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int maxn=1e5+6;
+int w[maxn];
+float v[maxn];
+float s[maxn];
+int m;
+int n,bags;
+
+int main()
+{
+    cin>>n>>bags;
+    for(int i=1;i<=n;i++)
+    {
+        cin>>w[i];
+    }
+    for(int i=1;i<=n;i++)
+    {
+        cin>>v[i];
+    }
+    
+
+}
