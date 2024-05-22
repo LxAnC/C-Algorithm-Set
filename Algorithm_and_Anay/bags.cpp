faa9ea5c9b80cda0e9ba85fdf990679d913cd64e
LxAnC <5001363@qq.com>
2024-05-22 22:45:22 +0800

    修改一下

--- Algorithm_and_Anay/bags.cpp
+++ Algorithm_and_Anay/bags.cpp
@@ -1,18 +1,28 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-const int maxn=1e5+10;
-typedef struct {
+const int maxn = 1e5 + 10;
+typedef struct Bags
+{
     double s;
     int weight;
     int value;
-}*Bags;
+} Bags;
 
 int main()
 {
-    int n,m;
-    Bags b[maxn]=new Bags[maxn];
-    cin>>n>>m;
+    int n, m;
+    Bags b[maxn];
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+        cin >> b[i].value;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> b[i].weight;
+        b[i].s = b[i].weight / b[i].value;
+    }
+    sort(b, b + n, [b](int i,int j){
+        return b[i].s>b[j].s
+    });
     for(int i=0;i<n;i++)
-    cin>>b[i].value;
-
+    
 }
