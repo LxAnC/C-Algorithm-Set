cdfcaa2c3a854886042dfa4cf3dd15e4c02b3b2b
LxAnC <5001363@qq.com>
2024-05-22 15:39:48 +0800

    切比雪夫距离的代码

--- Distance/Chebyshev.cpp
+++ Distance/Chebyshev.cpp
@@ -2,7 +2,8 @@
 // #include <math>
 #include <algorithm>
 using namespace std;
-
+//切比雪夫距离的实现
+//也就是
 int ChebyshevDistance(int x1, int x2, int y1, int y2)
 {
     return max(abs(x1 - x2), abs(y1 - y2));
