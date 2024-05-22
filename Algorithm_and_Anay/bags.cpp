aa0dec4afb5c5a41b586ea2f8f68b98d9338b92d
LxAnC <5001363@qq.com>
2024-05-22 18:03:56 +0800

    分数背包求解

--- Algorithm_and_Anay/bags.cpp
+++ Algorithm_and_Anay/bags.cpp
@@ -1,23 +1,49 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-const int maxn=1e5+6;
+const int maxn = 1e5 + 6;
 int w[maxn];
 float v[maxn];
 float s[maxn];
 int m;
-int n,bags;
-
+int n, bags;
 int main()
 {
-    cin>>n>>bags;
-    for(int i=1;i<=n;i++)
+    cin >> n >> bags;
+    for (int i = 0; i <n; i++)
+        cin >> v[i];
+    for (int i = 1; i <n; i++)
+    {cin >> w[i];s[i]=w[i]/v[i];}
+    sort(v,v+n,[&](int i,int j){
+        return s[i]>s[j]?swap(s[i],s[j]):0;
+    });
+    // sort(w,w+n,[&](int i,int j){
+    //     return s[i]>s[j];
+    // });
+    // sort(s,s+n,[&](int i,int j){
+    //     return s[i]>s[j];
+    // });
+    int c = bags;
+    float ret = 0;
+    int i;
+    for (i = 0; i <n&&c!=0; i++)
     {
-        cin>>w[i];
+        if (c > w[i])
+        {
+            ret += v[i];
+            c -= w[i];
+        }
+        else
+            break;
     }
-    for(int i=1;i<=n;i++)
-    {
-        cin>>v[i];
-    }
-    
-
+    for(int i=0;i<n;i++)
+    cout<<s[i]<<" "<<w[i]<<" "<<v[i]<<endl;
+    //装满的时候
+    cout<<ret<<endl;
+    cout<<s[i]<<endl;
+    if (i <= n&&c!=0)
+        ret += c * s[i];
+    cout << "最大价值" << endl;
+    cout << ret << endl;
+    cout << "最后一次容量" << c << endl;
+    return 0;
 }
