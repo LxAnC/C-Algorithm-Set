a7a618c3aeb07783d9c62da052b68e58f19ffbf1
LxAnC <5001363@qq.com>
2024-05-22 22:49:10 +0800

    有问题

--- Algorithm_and_Anay/bags.cpp
+++ Algorithm_and_Anay/bags.cpp
@@ -3,15 +3,19 @@ using namespace std;
 const int maxn = 1e5 + 10;
 typedef struct Bags
 {
-    double s;
+    int s;
     int weight;
     int value;
 } Bags;
-
+Bags b[maxn];
+bool cmp(int i,int j)
+{
+    return b[i].s>b[j].s;
+}
 int main()
 {
     int n, m;
-    Bags b[maxn];
+
     cin >> n >> m;
     for (int i = 0; i < n; i++)
         cin >> b[i].value;
@@ -20,9 +24,13 @@ int main()
         cin >> b[i].weight;
         b[i].s = b[i].weight / b[i].value;
     }
-    sort(b, b + n, [b](int i,int j){
-        return b[i].s>b[j].s
-    });
-    for(int i=0;i<n;i++)
-    
+    sort(b, b + n, cmp);
+    for (int i = 0; i < n; i++)
+    {
+        cout << b[i].s << endl;
+        cout << b[i].value;
+        cout << b[i].weight;
+        cout << endl;
+    }
+    return 0;
 }
