fe96d661cae52cbe0b52b5aec764c2e9622ab046
LxAnC <5001363@qq.com>
2024-05-23 23:20:15 +0800

    update

--- BIT/BIT.cpp
+++ BIT/BIT.cpp
@@ -6,90 +6,56 @@
 // https://zhuanlan.zhihu.com/p/93795692
 // 还有这篇https://www.cnblogs.com/Last--Whisper/p/13823614.html#%E4%BB%80%E4%B9%88%E6%98%AF-bit-
 // https://blog.csdn.net/TheWayForDream/article/details/118436732
-#include <bits/stdc++.h>
-using namespace std;
-#define MAXN 50005
-#define lowbit(x) ((x) & (-x))
 int tree[MAXN];
-void Update(int i, int x)
+inline void update(int i, int x)
 {
-    for (int i = i; i <= MAXN; i += lowbit(i))
-    {
-        tree[i] += x; // 这里如果对减少人数单独列一个函数更麻烦
-    }
+    for (int pos = i; pos < MAXN; pos += lowbit(pos))//这里对分别的阵营做更新
+        tree[pos] += x;//表示0-pos的总人数
 }
-inline int query(int x)
+inline int query(int n)
 {
-    int ret = 0;
-    for (int i = x; i; i -= lowbit(i))
-        ret += tree[i];
-    return ret;
+    int ans = 0;
+    for (int pos = n; pos; pos -= lowbit(pos))//遍历所有区域的总人数
+        ans += tree[pos];//分别把各段的加起来
+    return ans;
 }
-inline int query(int x, int y)
+inline int query(int a, int b)
 {
-    return tree[x] - tree[y - 1];
+    return query(b) - query(a - 1);//因为b-a+1是总距离，所以b-  a-1就是合理的减数
 }
-
 int main()
 {
-    int n, a, b, x;
-    cin >> n;
-    for (int i = 1; i <= n; i++)
+    int cases;
+    scanf("%d", &cases);
+    for (int I = 1; I <= cases; ++I)
     {
-        Update(i, cin >> x);
+        memset(tree, 0, sizeof(tree));
+        int n, x, a, b;
+        char opr[10];
+        printf("Case %d:\n", I);
+        scanf("%d", &n);
+        for (int i = 1; i <= n; ++i)
+        {
+            scanf("%d", &x);
+            update(i, x);
+        }
+        while (scanf("%s", opr), opr[0] != 'E')
+        {
+            switch (opr[0])
+            {
+            case 'A':
+                scanf("%d%d", &a, &b);
+                update(a, b);
+                break;
+            case 'S':
+                scanf("%d%d", &a, &b);
+                update(a, -b);
+                break;
+            case 'Q':
+                scanf("%d%d", &a, &b);
+                printf("%d\n", query(a, b));
+            }
+        }
     }
+    return 0;
 }
-
-// int tree[MAXN];
-// inline void update(int i, int x)
-// {
-//     for (int pos = i; pos < MAXN; pos += lowbit(pos))//这里对分别的阵营做更新
-//         tree[pos] += x;//表示0-pos的总人数
-// }
-// inline int query(int n)
-// {
-//     int ans = 0;
-//     for (int pos = n; pos; pos -= lowbit(pos))//遍历所有区域的总人数
-//         ans += tree[pos];//分别把各段的加起来
-//     return ans;
-// }
-// inline int query(int a, int b)
-// {
-//     return query(b) - query(a - 1);//因为b-a+1是总距离，所以b-  a-1就是合理的减数
-// }
-// int main()
-// {
-//     int cases;
-//     scanf("%d", &cases);
-//     for (int I = 1; I <= cases; ++I)
-//     {
-//         memset(tree, 0, sizeof(tree));
-//         int n, x, a, b;
-//         char opr[10];
-//         printf("Case %d:\n", I);
-//         scanf("%d", &n);
-//         for (int i = 1; i <= n; ++i)
-//         {
-//             scanf("%d", &x);
-//             update(i, x);
-//         }
-//         while (scanf("%s", opr), opr[0] != 'E')
-//         {
-//             switch (opr[0])
-//             {
-//             case 'A':
-//                 scanf("%d%d", &a, &b);
-//                 update(a, b);
-//                 break;
-//             case 'S':
-//                 scanf("%d%d", &a, &b);
-//                 update(a, -b);
-//                 break;
-//             case 'Q':
-//                 scanf("%d%d", &a, &b);
-//                 printf("%d\n", query(a, b));
-//             }
-//         }
-//     }
-//     return 0;
-// }
