f53405596212e55569fd22633576d60c37b9e6e7
LxAnC <5001363@qq.com>
2024-05-25 14:22:41 +0800

    更新一波c++的代码模版

---
+++ ExponentiationBySquaring/1.cpp
@@ -0,0 +1,21 @@
+//
+   //* @Author: LxAnC
+   //* @Email : LxAnC76@gmail.com
+   //* @Date  : 2024-05-25 14:22
+//  
+#include <bits/stdc++.h>
+using namespace std;
+#define N 1e5+5
+#define f1(i)  for(int i=1;i<=n;i++)
+#define f2(i)  for(int i=n;i;i--)
+#define first f
+#define second s
+#define max ma
+#define min mi
+
+int n,m,x,y,z,ans;
+
+int main()
+{
+   return 0;
+}
