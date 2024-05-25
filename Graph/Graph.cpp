e4f6661aec690ce39d7119938035ebeb6039c716
LxAnC <5001363@qq.com>
2024-05-25 12:42:06 +0800

    链式前向星

--- Graph/Graph.cpp
+++ Graph/Graph.cpp
@@ -5,23 +5,36 @@ using namespace std;
 // 无向图
 // 邻接矩阵
 int Nog[N][N]; // 不过达到10000（1e4）就不行了
-               // 邻接表的做法
-struct edge
-{
-    int to, w;
+
+
+// 邻接表的做法
+// struct edge
+// {
+//     int to, w;
+// };
+// vector<edge> nums[N];
+
+//第三种
+//链式前向星
+struct edge{
+    int to,w,next;
 };
-vector<edge> nums[N];
+int head[N];
 
-int n, m;
-int x, y, w;
-int main()
-{
-    cin >> n >> m;
-    for (int i = 1; i <= n; i++)
-    {
-        cin >> x >> y >> w;
-        Nog[x][y] = Nog[y][x] = w; // 初始化无向图；
-    }
 
-    return 0;
-}
+
+
+//邻接矩阵存无向图
+// int n, m;
+// int x, y, w;
+// int main()
+// {
+//     cin >> n >> m;
+//     for (int i = 1; i <= n; i++)
+//     {
+//         cin >> x >> y >> w;
+//         Nog[x][y] = Nog[y][x] = w; // 初始化无向图；
+//     }
+
+//     return 0;
+// }
