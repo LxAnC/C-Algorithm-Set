e8d4e052d7aab6381c2d1a48a7e6d7fa108372ee
LxAnC <5001363@qq.com>
2024-05-26 13:14:33 +0800

    首先举例多算的次数

--- ExponentiationBySquaring/1.cpp
+++ ExponentiationBySquaring/1.cpp
@@ -16,6 +16,23 @@ int n, m, x, y, z, ans;
 // 平方求幂环节
 int main()
 {
+   n=7;
+   //求n的10次方
+   //方法一
+   // cout<<n*n*n*n*n*n*n*n*n*n<<endl;
+   //上述方法会算10次
+
+   //方法二
+   // int b=n*n*n*n*n;
+   // cout<<b*b;
+   //上述方法会算5次
+
+   //方法三
+   // int c=n*n;
+   // int b=c*c*n;
+   // cout<<b*b<<endl;
+   //只用算四次
+
 
    return 0;
 }
