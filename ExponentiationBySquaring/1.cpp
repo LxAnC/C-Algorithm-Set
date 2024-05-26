255579ecd870531760e9d0f3d3abea77fd61658e
LxAnC <5001363@qq.com>
2024-05-26 13:20:02 +0800

    排列一下

--- ExponentiationBySquaring/1.cpp
+++ ExponentiationBySquaring/1.cpp
@@ -14,39 +14,41 @@ using namespace std;
 #define min mi
 int n, m, x, y, z, ans;
 // 平方求幂环节
+// 先看下面的34行
+
+// 所以这里使用递归快速幂来求幂达到O(logn)的级别
+void qpow(int a, int n)
+{
+   if (n == 0)
+      return 1;
+   else if (n % 2 == 0)
+   {
+      int temp = qpow(a, n / 2);
+      return temp * temp;
+   }
+   else
+      return qpow(a, n - 1) * a;
+}
+
 int main()
 {
-   n=7;
-   //求n的10次方
-   //方法一
-   // cout<<n*n*n*n*n*n*n*n*n*n<<endl;
-   //上述方法会算10次
+   n = 7;
+   // 求n的10次方
+   // 方法一
+   //  cout<<n*n*n*n*n*n*n*n*n*n<<endl;
+   // 上述方法会算10次
 
-   //方法二
-   // int b=n*n*n*n*n;
-   // cout<<b*b;
-   //上述方法会算5次
+   // 方法二
+   //  int b=n*n*n*n*n;
+   //  cout<<b*b;
+   // 上述方法会算5次
 
-   //方法三
-   // int c=n*n;
-   // int b=c*c*n;
-   // cout<<b*b<<endl;
-   //只用算四次
-
-
-   //所以这里使用递归快速幂来求幂达到O(logn)的级别
-   void qpow(int a,int n)
-   {
-      if(n==0)
-      return 1;
-      else if(n%2==0)
-      {
-         int temp=qpow(a,n/2);
-         return temp*temp;
-      }
-      else 
-      return qpow(a,n-1)*a;
-   }
+   // 方法三
+   //  int c=n*n;
+   //  int b=c*c*n;
+   //  cout<<b*b<<endl;
+   // 只用算四次
+   // 转上
 
    return 0;
 }
