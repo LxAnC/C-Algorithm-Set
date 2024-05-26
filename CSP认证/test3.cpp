d3b6c7eae13f9c6a1b56471209957b9ea19b85e4
LxAnC <5001363@qq.com>
2024-05-26 23:20:53 +0800

    化学方程式配平

---
+++ CSP认证/test3.cpp
@@ -0,0 +1,22 @@
+//
+//* @Author: LxAnC
+//* @Email : LxAnC76@gmail.com
+//* @Date  : 2024-05-26 23:20
+//
+#include <bits/stdc++.h>
+using namespace std;
+#define N 1e5 + 5
+#define f1(i) for (int i = 1; i <= n; i++)
+#define f2(i) for (int i = n; i; i--)
+#define first f
+#define second s
+#define max ma
+#define min mi
+
+int n, m, x, y, z, ans;
+
+int main()
+{
+
+    return 0;
+}
