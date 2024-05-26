cd9defbae7234005c6d7afa38e44e41aed4cb334
LxAnC <5001363@qq.com>
2024-05-26 13:10:25 +0800

    20240526平方求幂

--- ExponentiationBySquaring/1.cpp
+++ ExponentiationBySquaring/1.cpp
@@ -1,22 +1,21 @@
 //
-   //* @Author: LxAnC
-   //* @Email : LxAnC76@gmail.com
-   //* @Date  : 2024-05-25 14:24
-//  
+//* @Author: LxAnC
+//* @Email : LxAnC76@gmail.com
+//* @Date  : 2024-05-25 14:24
+//
 #include <bits/stdc++.h>
 using namespace std;
-#define N 1e5+5
-#define f1(i)  for(int i=1;i<=n;i++)
-#define f2(i)  for(int i=n;i;i--)
+#define N 1e5 + 5
+#define f1(i) for (int i = 1; i <= n; i++)
+#define f2(i) for (int i = n; i; i--)
 #define first f
 #define second s
 #define max ma
 #define min mi
-
-int n,m,x,y,z,ans;
-
+int n, m, x, y, z, ans;
+// 平方求幂环节
 int main()
 {
-   
+
    return 0;
 }
