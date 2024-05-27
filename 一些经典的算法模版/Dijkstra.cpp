6b976ba1240c1d6fd4a4378e50f6948e0abffc76
LxAnC <5001363@qq.com>
2024-05-27 11:54:29 +0800

    新添加数据

--- 一些经典的算法模版/Dijkstra.cpp
+++ 一些经典的算法模版/Dijkstra.cpp
@@ -1,14 +1,64 @@
-//迪杰斯特拉的代码模版
-//自己手写的
-//单源最短路径
+// 迪杰斯特拉的代码模版
+// 自己手写的
+// 单源最短路径
 #include <iostream>
 #define int long long
 using namespace std;
 const int maxn = 1e3 + 10;
-int n,m;
-int dist[maxn];//代表距离
-int g[maxn][maxn];//地图
-bool st[maxn];//代表有没有存过
+int n, m;          // 节点和边
+int dist[maxn];    // 代表距离
+int g[maxn][maxn]; // 地图
+bool st[maxn];     // 代表有没有存过
+
+//数据
+// 6 10
+// 1 2 4
+// 1 3 8
+// 2 3 2
+// 2 4 3
+// 3 4 2
+// 2 5 6
+// 3 6 8
+// 4 5 3
+// 4 6 1
+// 5 6 6
+// void dijkstra()
+// {
+//     memset(dist, 0x3f, sizeof(dist));
+//     dist[1] = 0;
+//     int elem = n;
+//     while (elem--)
+//     {
+//         int t = -1;
+//         for (int i = 1; i <= n; i++)
+//         {
+
+//             if (st[i] == 0 && (t == -1) || dist[i] < dist[t])
+//             {
+//                 t = i;
+//             }
+//             st[t] = true;
+//             for (int i = 1; i <= n; i++)
+//             if(st[i]==0)
+//                 dist[i] = min(dist[t] + g[t][i], dist[i]);
+//         }
+//     }
+// }
+
+// signed main()
+// {
+//     cin>>n>>m;
+//     for (int i = 1; i <= m; i++)
+//     {
+//         int x, y, z;
+//         cin >> x >> y >> z;
+//         g[x][y] = z;
+//     }
+//     dijkstra();
+//     cout << "单源最短路径如下" << endl;
+//     for (int i = 1; i <= n; i++)
+//         cout << dist[i] << endl;
+// }
 
 void dijkstra()
 {
@@ -45,10 +95,10 @@ signed main()
         g[x][y]=min(g[x][y],z);
     }
     dijkstra();
-    // for (int i = 1; i <= n; i++)
-    // {
-    //     cout << dist[i];
-    //     cout << endl;
-    // }//输出所有的距离
-    cout<<dist[n]<<endl;//最后一个即我们需要的值
+    for (int i = 1; i <= n; i++)
+    {
+        cout << dist[i];
+        cout << endl;
+    }//输出所有的距离
+    // cout<<dist[n]<<endl;//最后一个即我们需要的值
 }
