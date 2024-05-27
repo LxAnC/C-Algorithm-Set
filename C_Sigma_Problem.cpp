ff7e1036ef59a7eaec9d6ae67cd0e714d86bd9aa
LxAnC <5001363@qq.com>
2024-05-27 23:48:38 +0800

    update

--- C_Sigma_Problem.cpp
+++ C_Sigma_Problem.cpp
@@ -1,9 +1,10 @@
 //
 //* @Author: LxAnC
 //* @Email : LxAnC76@gmail.com
-//* @Date  : 2024-05-27 21:43
+//* @Date  : 2024-05-27 22:14
 //
 #include <bits/stdc++.h>
+
 using namespace std;
 #define int long long
 #define N 1000005
@@ -13,18 +14,16 @@ using namespace std;
 #define second s
 #define max ma
 #define min mi
-#define Mod (int)1e8
-int n, m, x, y, z, ans;
 int a[N];
-
-int check(int l, int r, int cnt)
+int n, m, x, y, z, ans;
+int binarysearch(int l, int r,int c1)
 {
-    if (cnt > a[r])
+    if (a[r] < c1)
         return r + 1;
     while (l < r)
     {
         int m = l + r >> 1;
-        if (a[m] >= cnt)
+        if (a[m] >= c1)
             r = m;
         else
             l = m + 1;
@@ -33,19 +32,21 @@ int check(int l, int r, int cnt)
 }
 signed main()
 {
+    int ret = 0;
     cin >> n;
-    int sum = 0;
-    for (int i = 1; i <= n; i++)
-        cin >> a[i];
-    int go=0;
-    sort(a + 1, a + n + 1);
     for (int i = 1; i <= n; i++)
     {
-        sum += a[i];
-        int g = n - check(i + 1, n, 100000000 - a[i]) + 1;
-        go += g;
+        cin >> a[i];
     }
-    cout << sum * (n - 1) - 100000000 * go;
-
+    sort(a + 1, a + n + 1);
+    int gs = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        ret += a[i];
+        int b=100000000-a[i];
+        int a = n  - binarysearch(i + 1, n, b)+1;
+        gs += a;
+    }
+    cout << ret * (n - 1) - 100000000 * gs;
     return 0;
 }
