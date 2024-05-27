db50726cfdf126716e215ce6a815d3c790ef0b51
LxAnC <5001363@qq.com>
2024-05-27 11:59:42 +0800

    20240527留

---
+++ 一些经典的算法模版/SPFA算法.cpp
@@ -0,0 +1,22 @@
+//
+   //* @Author: LxAnC
+   //* @Email : LxAnC76@gmail.com
+   //* @Date  : 2024-05-27 11:59
+//  
+#include <bits/stdc++.h>
+using namespace std;
+#define N 10005
+#define f1(i)  for(int i=1;i<=n;i++)
+#define f2(i)  for(int i=n;i;i--)
+#define first f
+#define second s
+#define max ma
+#define min mi
+
+int n,m,x,y,z,ans;
+
+int main()
+{
+   
+   return 0;
+}
