32167dabebbeecc8d5ae0c3114a69ad9e6add7dc
LxAnC <5001363@qq.com>
2024-05-28 23:36:15 +0800

    操作系统的实验

--- 操作系统/请求分页存储管理LRU.cpp
+++ 操作系统/请求分页存储管理LRU.cpp
@@ -1,6 +1,6 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-//内存块类
+// 内存块类
 class memory
 {
 public:
@@ -13,23 +13,24 @@ public:
     void refresh();
     friend int manage(memory *My_Memory, int ans, int a);
     friend void show(memory *My_Memory, int ans);
+
 private:
     int page;
     int time;
 };
-//为了多次初始化
+// 为了多次初始化
 void memory::init()
 {
     page = 0;
     time = 0;
 }
-//修改内存块保存的页和时间
+// 修改内存块保存的页和时间
 void memory::alter(int a, int b)
 {
     page = a;
     time = b;
 }
-//检查该块是否已被占用
+// 检查该块是否已被占用
 int memory::check_full()
 {
     if (page == 0 && time == 0)
@@ -41,12 +42,12 @@ int memory::check_full()
         return 0;
     }
 }
-//检查该块的驻留时间
+// 检查该块的驻留时间
 int memory::check_old()
 {
     return time;
 }
-//检查该块中是否保存了页a
+// 检查该块中是否保存了页a
 int memory::check_exist(int a)
 {
     if (page == a)
@@ -58,7 +59,7 @@ int memory::check_exist(int a)
         return 0;
     }
 }
-//时间++
+// 时间++
 void memory::run()
 {
     if (page != 0)
@@ -66,15 +67,15 @@ void memory::run()
         time++;
     }
 }
-//时间置0
+// 时间置0
 void memory::refresh()
 {
     time = 0;
 }
-//调度作业
+// 调度作业
 int manage(memory *My_Memory, int ans, int a)
 {
-    //检查内存块是否已经保存了页面a
+    // 检查内存块是否已经保存了页面a
     for (int i = 0; i < ans; i++)
     {
         if (My_Memory[i].check_exist(a))
@@ -84,7 +85,7 @@ int manage(memory *My_Memory, int ans, int a)
             return 1;
         }
     }
-    //检查内存块是否有空闲页面
+    // 检查内存块是否有空闲页面
     for (int i = 0; i < ans; i++)
     {
         if (My_Memory[i].check_full())
@@ -94,12 +95,12 @@ int manage(memory *My_Memory, int ans, int a)
             return 0;
         }
     }
-    //找到驻留时间最大的块，进行替换
+    // 找到驻留时间最大的块，进行替换
     int max_time = 0;
     int num = -1;
     for (int i = 0; i < ans; i++)
     {
-        if (My_Memory[i].check_old()>max_time)
+        if (My_Memory[i].check_old() > max_time)
         {
             max_time = My_Memory[i].check_old();
             num = i;
@@ -109,7 +110,7 @@ int manage(memory *My_Memory, int ans, int a)
     My_Memory[num].alter(a, 0);
     return 0;
 }
-//显示内存块当前状态
+// 显示内存块当前状态
 void show(memory *My_Memory, int ans)
 {
     cout << "...................................." << endl;
@@ -128,7 +129,8 @@ void show(memory *My_Memory, int ans)
     {
         cout << "  |" << My_Memory[i].time;
     }
-    cout << "\n....................................\n" << endl;
+    cout << "\n....................................\n"
+         << endl;
 }
 int main()
 {
@@ -148,19 +150,19 @@ int main()
         cin >> flag;
         if (flag == 1)
         {
-            //设置存储区块数
+            // 设置存储区块数
             cout << "请输入存储区块数：";
             cin >> ans;
             for (int i = 0; i < ans; i++)
             {
-                My_Memory[i].init();  
+                My_Memory[i].init();
             }
             nohit = 0;
             aans = 0;
         }
         else if (flag == 2)
         {
-            //页面调度模拟
+            // 页面调度模拟
             while (true)
             {
                 for (int i = 0; i < ans; i++)
@@ -181,19 +183,19 @@ int main()
                 }
                 show(My_Memory, ans);
             }
- 
         }
         else if (flag == 3)
         {
-            //缺页率统计
+            // 缺页率统计
             cout << "*****************" << endl;
             cout << "调度作业 " << aans << "次\n中断     " << nohit << "次\n缺页率   " << double(nohit) / aans * 100.00 << "%" << endl;
             cout << "*****************" << endl;
         }
         else if (flag == 4)
         {
-            //退出
-            cout << "成功退出！\n" << endl;
+            // 退出
+            cout << "成功退出！\n"
+                 << endl;
             break;
         }
     }
