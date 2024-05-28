194979aeb86b202559aa79592411bd333a600380
LxAnC <5001363@qq.com>
2024-05-28 15:41:27 +0800

    做法更新

--- D_Another_Sigma_Problem.cpp
+++ D_Another_Sigma_Problem.cpp
@@ -1,57 +1,45 @@
-#include <bits/stdc++.h>
-#define int long long
-const int m = 998244353;
-const int N = (int)2e5 + 10;
-using namespace std;
-int n, m, a[N], b[N], c[N];
-signed main()
+#include <cstdio>
+
+const int MAXN = 2e5 + 5;
+const long long int mod = 998244353;
+int n;
+long long int ans, a[MAXN], suf[MAXN], suf2[MAXN]; // 用两个数组分别存储两部分答案
+inline long long int get_len(long long int x); // 获取数字长度
+
+int main(void)
 {
-    cin >> n;
-    int ret=0; 
-    for (int i = 1; i <= n; i++)
-    {
-        cin >> a[i];
-        string s;
-        s = to_string(a[i]);
-        c[s.size()]++;//长度加一
-        b[i]=s.size();//a的长度给到B，算A的贡献
-    }
-    for(int i=1;i<=n;i++)
-    {
-        c[b[i]]--;//放串
-        for(int j=1;j<=9;j++)
-        {
-            
-        }
-    }
+	scanf("%d", &n);
+	
+	for (int i = 1; i <= n; i++)
+	{
+		scanf("%lld", &a[i]);
+	}
+	
+	for (int i = n; i >= 2; i--)
+	{
+		suf[i] = (suf[i + 1] + a[i]) % mod; // 计算时记得取模
+		suf2[i] = (suf2[i + 1] + get_len(a[i])) % mod; // 同上
+	}
+	
+	for (int i = 1; i < n; i++)
+	{
+		ans += suf[i + 1];
+		ans += a[i] * suf2[i + 1] % mod;
+		ans %= mod; // 根据上面的余数可加性，记得每次都取个模
+	}
+	
+	printf("%lld\n", ans);
+	return 0;
 }
-// #include<bits/stdc++.h>
-// using namespace std;
-// const int mod = 998244353;
-// int a[200010],b[200010],c[10];
-// signed main()
-// {
-//     int n;
-//     cin>>n;
-//     for(int i = 1;i <= n;i++)
-//     {
-//         cin>>a[i];
-//         string s1 = to_string(a[i]);
-//         c[s1.size()]++;
-//         b[i] = a[i].size();//存下来 a[i] 的长度
-//     }
-//     long long ans = 0;
-//     for(int i = 1;i <= n;i++)
-//     {
-//         c[b[i]]--;//减去a[i]的长度，避免重复计算
-//         for(int z = 1;z <= 9;z++)//a[i] 作为第一个操作数的贡献
-//         {
-            ans += (((a[i]*((long long)pow(10,z)%mod))%mod)*zhi)%mod;//注意步步取模，不然就挂了
-//             ans%=mod;
-//         }
-//         ans += (a[i]*(i-1))%mod;//第二种情况的贡献
-//         ans%=mod;
-//     }
-//     cout<<ans<<"\n";
-//     return 0;
-// }
+
+inline long long int get_len(long long int x)
+{
+	long long int len = 1;
+	
+	while (x > 0)
+	{
+		len = len * 10 % mod, x /= 10;
+	}
+	
+	return len;
+}
