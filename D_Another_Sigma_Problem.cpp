d3fdde0cd7a86264e6cd2c3a48d44c1a035c99f5
LxAnC <5001363@qq.com>
2024-05-28 16:11:15 +0800

    20240528留

--- D_Another_Sigma_Problem.cpp
+++ D_Another_Sigma_Problem.cpp
@@ -1,45 +1,37 @@
-#include <cstdio>
+//
+//* @Author: LxAnC
+//* @Email : LxAnC76@gmail.com
+//* @Date  : 2024-05-28 16:07
+//
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define maxn (int)2e6 + 5
+#define f1(i) for (int i = 1; i <= n; i++)
+#define f2(i) for (int i = n; i; i--)
+#define first f
+#define second s
+#define max ma
+#define min mi
 
-const int MAXN = 2e5 + 5;
-const long long int mod = 998244353;
-int n;
-long long int ans, a[MAXN], suf[MAXN], suf2[MAXN]; // 用两个数组分别存储两部分答案
-inline long long int get_len(long long int x); // 获取数字长度
-
-int main(void)
+int n, m, x, y, z, ans;
+int a[maxn], cnt[11];
+signed main()
 {
-	scanf("%d", &n);
-	
-	for (int i = 1; i <= n; i++)
-	{
-		scanf("%lld", &a[i]);
-	}
-	
-	for (int i = n; i >= 2; i--)
-	{
-		suf[i] = (suf[i + 1] + a[i]) % mod; // 计算时记得取模
-		suf2[i] = (suf2[i + 1] + get_len(a[i])) % mod; // 同上
-	}
-	
-	for (int i = 1; i < n; i++)
-	{
-		ans += suf[i + 1];
-		ans += a[i] * suf2[i + 1] % mod;
-		ans %= mod; // 根据上面的余数可加性，记得每次都取个模
-	}
-	
-	printf("%lld\n", ans);
-	return 0;
-}
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+        cnt[to_string(a[i]).size()]++;
+    }
 
-inline long long int get_len(long long int x)
-{
-	long long int len = 1;
-	
-	while (x > 0)
-	{
-		len = len * 10 % mod, x /= 10;
-	}
-	
-	return len;
+    int ret=0;
+    for (int i = 1; i <= n; i++)
+    {
+        ret+=a[i]*(i-1);
+        for(int i=1;i<=10;i++)
+         ret+=a[i]*pow(10,i)+cnt[i];
+    }
+    cout<<ret;
+    return 0;
 }
