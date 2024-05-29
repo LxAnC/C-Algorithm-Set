af2445cb5a2656a05787d89104da312caa4bb5ff
LxAnC <5001363@qq.com>
2024-05-29 15:43:15 +0800

    E题题解

--- E_Yet_Another_Sigma_Problem.cpp
+++ E_Yet_Another_Sigma_Problem.cpp
@@ -1,65 +1,33 @@
-// //
-// //* @Author: LxAnC
-// //* @Email : LxAnC76@gmail.com
-// //* @Date  : 2024-05-29 14:50
-// //
-// #include <bits/stdc++.h>
-// using namespace std;
-// #define N 10005
-// #define f1(i) for (int i = 1; i <= n; i++)
-// #define f2(i) for (int i = n; i; i--)
-// #define first f
-// #define second s
-// #define max ma
-// #define min mi
-// // 只用排序一下，做相同就行
-// int n, m, x, y, z, ans;
-// string a[N];
-// int main()
-// {
-//     cin >> n;
-//     for (int i = 1; i <= n; i++)
-//     {
-//         cin >> a[i];
-//     }
-//     sort(a + 1, a + n + 1);
-//     for (int i = 1; i <= n - 1; i++)
-//         for (int j = i + 1; j <= n; j++)
-//         {
-//             if(a[i][0]!=a[j][0])
-//             break;
-//             int len=1;
-//             int z = 1;
-//             while (a[i][z] == a[j][z])
-//             {
-//                 len++;z++;
-//             }
-//             ans+=len;
-//         }
-//     cout << ans << endl;
-//     return 0;
-// }
+//
+//* @Author: LxAnC
+//* @Email : LxAnC76@gmail.com
+//* @Date  : 2024-05-29 15:33
+//
 #include <bits/stdc++.h>
 using namespace std;
-const int N = 3e5 + 10;
-#define ll long long
-int n, tot;
-char s[N];
-int trie[N][50];
-ll ans, word[N];
-void insert(char c[])
+#define N 10005
+#define f1(i) for (int i = 1; i <= n; i++)
+#define f2(i) for (int i = n; i; i--)
+#define first f
+#define second s
+#define max ma
+#define min mi
+
+int n, m, x, y, z, ans, tot;
+int trie[10000][26];
+char ch[N];
+int word[N];
+void insert(char s[])
 {
     int u = 0;
-    int len = strlen(c);
+    int len = strlen(s);
     for (int i = 0; i < len; i++)
     {
         word[u]++;
-        int a = c[i] - 'a';
-        if (trie[u][a] == 0)
-        {
-            trie[u][a] = ++tot;
-        }
-        u = trie[u][a];
+        int c = s[i] - 'a';
+        if (trie[u][c] == 0)
+            trie[u][c] = ++tot; // 添加节点
+        u = trie[u][c];         // 更新下一个节点
     }
     word[u]++;
 }
@@ -68,13 +36,13 @@ int main()
     cin >> n;
     for (int i = 1; i <= n; i++)
     {
-        cin >> s;
-        insert(s);
+        cin >> ch;
+        insert(ch);
     }
-    for (int i = 1; i <= tot; i++)
-    {
-        ans += word[i] * (word[i] - 1) / 2;
-    }
-    cout << ans;
+    for(int i=1;i<=tot;i++)
+    ans+=word[i]*(word[i]-1)/2;
+    cout<<ans;
     return 0;
 }
+
+// 字典树的学习
