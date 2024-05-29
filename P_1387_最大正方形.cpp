65183c56ebab40f5dcbcf2d26e4f28c80e1a4375
LxAnC <5001363@qq.com>
2024-05-29 22:33:25 +0800

    自己模拟一遍

--- P_1387_最大正方形.cpp
+++ P_1387_最大正方形.cpp
@@ -1,33 +1,53 @@
 //
-//* @Author: LxAnC
-//* @Email : LxAnC76@gmail.com
-//* @Date  : 2024-05-29 17:52
-//
+   //* @Author: LxAnC
+   //* @Email : LxAnC76@gmail.com
+   //* @Date  : 2024-05-29 22:27
+//  
 #include <bits/stdc++.h>
 using namespace std;
-#define N 105
-#define f1(i) for (int i = 1; i <= n; i++)
-#define f2(i) for (int i = n; i; i--)
+#define N 1005
+#define f1(i)  for(int i=1;i<=n;i++)
+#define f2(i)  for(int i=n;i;i--)
 #define first f
 #define second s
 // #define max ma
 // #define min mi
+int startx,starty;
+int n,m,x,y,z,ans;
+int maxn;
 int a[N][N];
-int n, m, x, y, z, ans;
-int g[N][N];
+int dfs(int x,int y)
+{
+    if(x>n||y>n)
+    return 0;
+    for(int i=startx;i<=x;i++)
+      if(a[i][y]==0)
+        return 0;
+    for(int i=starty;i<=y;i++)
+      if(a[x][i]==0)
+       return 0;
+    ans++;
+    dfs(x+1,y+1);
+    return ans;
+}
+
+
+
 int main()
 {
-    cin >> n >> m;
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= m; j++)
+   cin>>n>>m;
+   for(int i=1;i<=n;i++)
+     for(int j=1;j<=m;j++)
+        cin>>a[i][j];
+    for(int i=1;i<=n;i++)
+      for(int j=1;j<=m;j++)
+        if(a[i][j])
         {
-            cin>>g[i][j];
-            if(g[i][j] == 1 )
-            a[i][j]=min(min(a[i - 1][j], a[i - 1][j - 1]), a[i][j - 1]) + 1 ;
-            ans = max(ans, a[i][j]);
+            startx=i;
+            starty=j;
+            ans=0;
+            maxn=max(maxn,dfs(i,j));
         }
-    }
-    cout << ans;
-    return 0;
+        cout<<maxn;
+   return 0;
 }
