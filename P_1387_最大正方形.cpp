cdaad69ba89d1ba4f22c784c3b2fe2873dfe2117
LxAnC <5001363@qq.com>
2024-05-29 22:27:49 +0800

    做一下动态规划的模版题

--- P_1387_最大正方形.cpp
+++ P_1387_最大正方形.cpp
@@ -1,22 +1,33 @@
 //
-   //* @Author: LxAnC
-   //* @Email : LxAnC76@gmail.com
-   //* @Date  : 2024-05-29 17:52
-//  
+//* @Author: LxAnC
+//* @Email : LxAnC76@gmail.com
+//* @Date  : 2024-05-29 17:52
+//
 #include <bits/stdc++.h>
 using namespace std;
-#define N 10005
-#define f1(i)  for(int i=1;i<=n;i++)
-#define f2(i)  for(int i=n;i;i--)
+#define N 105
+#define f1(i) for (int i = 1; i <= n; i++)
+#define f2(i) for (int i = n; i; i--)
 #define first f
 #define second s
-#define max ma
-#define min mi
-
-int n,m,x,y,z,ans;
-
+// #define max ma
+// #define min mi
+int a[N][N];
+int n, m, x, y, z, ans;
+int g[N][N];
 int main()
 {
-   
-   return 0;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            cin>>g[i][j];
+            if(g[i][j] == 1 )
+            a[i][j]=min(min(a[i - 1][j], a[i - 1][j - 1]), a[i][j - 1]) + 1 ;
+            ans = max(ans, a[i][j]);
+        }
+    }
+    cout << ans;
+    return 0;
 }
