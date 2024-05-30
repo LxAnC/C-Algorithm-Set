f35c7a4bf379e37e1c1562b1addc19e021078064
LxAnC <5001363@qq.com>
2024-05-30 23:56:30 +0800

    用这样做

--- B_AtCoder_Janken_2.cpp
+++ B_AtCoder_Janken_2.cpp
@@ -8,8 +8,7 @@ using namespace std;
 #define N 10005
 #define f1(i) for (int i = 1; i <= n; i++)
 #define f2(i) for (int i = n; i; i--)
-#define first f
-#define second s
+
 #define max ma
 #define min mi
 int n, m, x, y, z, ans;
@@ -17,6 +16,15 @@ int cnt=0;
 int main()
 {
     cin >> n;
-
+    string s;
+    int c;
+    int s=0;
+    vector<pair<string ,int>>a(n);
+    for(auto &i:a)
+    {
+        cin>>i.first;
+        cin>>i.second;
+        s+=i.second
+    }
     return 0;
 }
