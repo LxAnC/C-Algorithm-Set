31213e6d19443473be6dbb042cac5657e204db34
LxAnC <5001363@qq.com>
2024-05-30 23:51:32 +0800

    重新做

--- B_AtCoder_Janken_2.cpp
+++ B_AtCoder_Janken_2.cpp
@@ -12,36 +12,11 @@ using namespace std;
 #define second s
 #define max ma
 #define min mi
-typedef struct node{
-    string name;
-    int score;
-}Nums;
 int n, m, x, y, z, ans;
 int cnt=0;
-bool cmp(string s1,string s2)
-{
-    return s1<s2;
-}
 int main()
 {
     cin >> n;
-    int sum=0;
-    Nums nums[N];
-    map<string, int> hash;
-    int sum;
-    for (int i = 1; i <= n; i++)
-    {
-        string s;
-        int u;
-        cin >> nums[i].name;
-        cin >> nums[i].score;
-        // nums[i].pos=i;
-        sum+=nums[i].score;
-    }
-    sort(nums+1,nums+n+1,cmp);
-    int l=sum%n;
-    cout<<nums[l].name;
 
-    
     return 0;
 }
