bea8e96590e6b234b9f7fff5b369ad7eb1f4c968
LxAnC <5001363@qq.com>
2024-05-31 13:05:59 +0800

    更新代码模版和重新做了一下B题

--- B_AtCoder_Janken_2.cpp
+++ B_AtCoder_Janken_2.cpp
@@ -1,36 +1,50 @@
 //
 //* @Author: LxAnC
 //* @Email : LxAnC76@gmail.com
-//* @Date  : 2024-05-30 23:36
+//* @Date  : 2024-05-31 12:54
 //
 #include <bits/stdc++.h>
 using namespace std;
-#define N 10005
+#define int long long
+#define N 1e6 + 10
 #define f1(i) for (int i = 1; i <= n; i++)
 #define f2(i) for (int i = n; i; i--)
-
-#define max ma
-#define min mi
-int n, m, x, y, z, ans;
-int cnt = 0;
-int main()
+#define f first
+#define s second
+#define d max
+#define x min
+#define en '\n'
+int pow(int a, int n)
 {
+    int q = 1;
+    while (n)
+    {
+        if (n & 1)
+            q = q * a;
+        a *= a;
+        n = n >> 1;
+    }
+    return q;
+}
+
+int n, m, x, y, z, ans;
+
+signed main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     cin >> n;
-    string s;
-    int c;
-    int s = 0;
     vector<pair<string, int>> a(n);
     for (auto &i : a)
     {
-        cin >> i.first;
-        cin >> i.second;
-        s += i.second;
+        cin >> i.f >> i.s;
+        ans += i.s;
     }
     vector<int> id(n);
     iota(id.begin(), id.end(), 0);
-    sort(id.begin(), id.end(), [&](int x, int y)
-         { return a[x].first < a[y].first; });
-    int win = id[s % n];
-    cout << a[win].first;
+    sort(id.begin(),id.end(),[&](int i,int j){
+        return a[i].f<a[j].f;
+    });
+    ans=id[ans%n];
+    cout<<a[ans].f;
     return 0;
 }
