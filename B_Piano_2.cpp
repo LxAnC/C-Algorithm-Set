825a08c8d31134d5b0ebcea078747ceea2c055a4
LxAnC <5001363@qq.com>
2024-06-02 10:18:22 +0800

    ABC355B题

---
+++ B_Piano_2.cpp
@@ -0,0 +1,45 @@
+//
+//* @Author: LxAnC
+//* @Email : LxAnC76@gmail.com
+//* @Date  : 2024-06-02 10:10
+//
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define N (int)1e4+10
+#define f1(i) for (int i = 1; i <= n; i++)
+#define f2(i) for (int i = n; i; i--)
+#define f first
+#define s second
+#define d max
+#define x min
+#define en '\n'
+int pow(int a, int n)
+{
+    int q = 1;
+    while (n)
+    {
+        if (n & 1)
+            q = q * a;
+        a *= a;
+        n = n >> 1;
+    }
+    return q;
+}
+
+int n, m, x, y, z, ans;
+int a[N], b[N];
+signed main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+    for (int i = 1; i <= m; i++)
+        cin >> b[i];
+    for (int i = 1; i <= n; i++)
+        if (abs(a[i] - a[i - 1]) == 1&&a[i]!=1)
+         {   cout << "Yes" << en;return 0;}
+    cout<<"No"<<en;
+    return 0;
+}
