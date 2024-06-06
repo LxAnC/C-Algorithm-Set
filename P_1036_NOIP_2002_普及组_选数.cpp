ecfd1565b97c36c3aecd58dc8cf5640401e0d667
LxAnC <5001363@qq.com>
2024-06-06 23:25:10 +0800

    try

---
+++ P_1036_NOIP_2002_普及组_选数.cpp
@@ -0,0 +1,36 @@
+//
+   //* @Author: LxAnC
+   //* @Email : LxAnC76@gmail.com
+   //* @Date  : 2024-06-06 23:24
+//  
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define N 1e6+10
+#define f1(i)  for(int i=1;i<=n;i++)
+#define f2(i)  for(int i=n;i;i--)
+#define f first
+#define s second
+#define ma max
+#define mi min
+#define en '\n'
+int pow(int a , int n)
+{
+    int q = 1;
+    while(n)
+    {
+      if(n & 1)q = q * a;
+      a *= a;
+      n = n>>1;
+    }
+    return q;
+}
+
+int n,m,x,y,z,ans;
+
+signed main()
+{
+    ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+    
+    return 0;
+}
