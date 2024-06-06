abe13ad05b71990032492db14f51f63960a2869a
LxAnC <5001363@qq.com>
2024-06-06 23:27:14 +0800

    dfs框架

--- P_1036_NOIP_2002_普及组_选数.cpp
+++ P_1036_NOIP_2002_普及组_选数.cpp
@@ -1,36 +1,46 @@
 //
-   //* @Author: LxAnC
-   //* @Email : LxAnC76@gmail.com
-   //* @Date  : 2024-06-06 23:24
-//  
+//* @Author: LxAnC
+//* @Email : LxAnC76@gmail.com
+//* @Date  : 2024-06-06 23:24
+//
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
-#define N 1e6+10
-#define f1(i)  for(int i=1;i<=n;i++)
-#define f2(i)  for(int i=n;i;i--)
+#define N 1e6 + 10
+#define f1(i) for (int i = 1; i <= n; i++)
+#define f2(i) for (int i = n; i; i--)
 #define f first
 #define s second
 #define ma max
 #define mi min
 #define en '\n'
-int pow(int a , int n)
+int pow(int a, int n)
 {
     int q = 1;
-    while(n)
+    while (n)
     {
-      if(n & 1)q = q * a;
-      a *= a;
-      n = n>>1;
+        if (n & 1)
+            q = q * a;
+        a *= a;
+        n = n >> 1;
     }
     return q;
 }
 
-int n,m,x,y,z,ans;
-
+int n, m, x, y, z, ans;
+    vector<int> a(N);
+int dfs(int k,int sum,int start)
+{
+    
+}
 signed main()
 {
-    ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
-    
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int n, k;
+    cin >> n >> k;
+
+    for (int i = 1; i <= 4; i++)
+        cin >> a[i - 1];
+    dfs(0,0,0);
     return 0;
 }
