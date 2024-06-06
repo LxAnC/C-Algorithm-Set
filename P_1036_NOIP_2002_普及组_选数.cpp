01cf2a605667cfe227efadeabc3029a3ef64c801
LxAnC <5001363@qq.com>
2024-06-06 23:47:18 +0800

    洛谷题解

--- P_1036_NOIP_2002_普及组_选数.cpp
+++ P_1036_NOIP_2002_普及组_选数.cpp
@@ -1,64 +1,47 @@
-//
-//* @Author: LxAnC
-//* @Email : LxAnC76@gmail.com
-//* @Date  : 2024-06-06 23:24
-//
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
 using namespace std;
-#define int long long
-#define N 1e6 + 10
-#define f1(i) for (int i = 1; i <= n; i++)
-#define f2(i) for (int i = n; i; i--)
-#define f first
-#define s second
-#define ma max
-#define mi min
-#define en '\n'
-int pow(int a, int n)
-{
-    int q = 1;
-    while (n)
-    {
-        if (n & 1)
-            q = q * a;
-        a *= a;
-        n = n >> 1;
-    }
-    return q;
+
+bool isprime(int a){//判断素数
+    /*0和1特判真的没啥用对这题
+    吐槽：题中n的数据范围很奇怪，
+    n还有可能=1.....那k<n......
+    */
+    for(int i = 2;i * i <= a; i++)//不想用sqrt，还要头文件
+        if(a % i == 0)//如果整除
+            return false;//扔回false
+    //程序都到这里的话就说明此为素数
+    //否则就被扔回了
+    return true;//扔回true
 }
 
-int n, m, x, y, z, ans;
-vector<int> a(N);
-bool check(int sum)
-{
-    int i;
-    for (i = 2; i * i <= sum; i++)
-        if (sum % i == 0)
-            return false;
-    return true;
-}
-int dfs(int k, int sum, int start)
-{
-    if (k == y)
-    {
-        if (check(sum))
-            ans++;
-        return 0;
-    }
-    for (int i = start; i < n; i++)
-    {
-        dfs(k + 1, sum += a[i], i + 1);
-    }
-    return 0;
-}
-signed main()
-{
-    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    cin >> n >> y;
+int n,k;
+int a[25];
+long long ans;
 
-    for (int i = 1; i <= 4; i++)
-        cin >> a[i - 1];
-    dfs(0, 0, 0);
-    cout << ans;
-    return 0;
+void dfs(int m, int sum, int startx){//最重要的递归
+//m代表现在选择了多少个数
+//sum表示当前的和
+//startx表示升序排列，以免算重
+    if(m == k){//如果选完了的话
+        if(isprime(sum))//如果和是素数
+            ans++;//ans加一
+        return ;
+    }
+    for(int i = startx; i < n; i++)
+        dfs(m + 1, sum + a[i], i + 1);//递归
+        //步数要加一，和也要加
+        //升序起始值要变成i+1,以免算重
+    return ;//这一个步骤下，所有的都枚举完了
+    //直接返回去
+}
+
+int main(){
+    scanf("%d%d",&n,&k);//输入
+    
+    for(int i = 0; i < n; i++)
+        scanf("%d",&a[i]);//循环读入
+    dfs(0,0,0);//调用函数
+    printf("%d\n",ans);//输出答案
+    return 0;//结束程序
 }
