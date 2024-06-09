26bd78c182910dffe08f1249ee2896cc85018757
LxAnC <5001363@qq.com>
2024-06-09 21:31:56 +0800

    记忆话搜索的不一样的方式

--- P_1464_Function.cpp
+++ P_1464_Function.cpp
@@ -1,63 +1,63 @@
-//
-//* @Author: LxAnC
-//* @Email : LxAnC76@gmail.com
-//* @Date  : 2024-06-07 00:20
-//
-#include <bits/stdc++.h>
-using namespace std;
-#define int long long
-#define N 1e6 + 10
-#define f1(i) for (int i = 1; i <= n; i++)
-#define f2(i) for (int i = n; i; i--)
-#define f first
-#define s second
-#define ma max
-#define mi min
-#define en '\n'
-int pow(int a, int n)
+#include <cstdio>
+#define LL long long
+
+LL dp[25][25][25];
+
+LL w(LL a, LL b, LL c)
 {
-  int q = 1;
-  while (n)
-  {
-    if (n & 1)
-      q = q * a;
-    a *= a;
-    n = n >> 1;
-  }
-  return q;
+	if(a <= 0 || b <= 0 || c <= 0) return 1;//两个特判，题意里都有的。
+	if(a > 20 || b > 20 || c > 20) return w(20, 20, 20);
+	
+	if(a <b && b < c)//情况一，每一个没有储存过的“w”值全部储存，如果有就直接调用。
+	{
+		if(dp[a][b][c-1] == 0)
+		{
+			dp[a][b][c-1] = w(a, b, c-1);
+		}
+		if(dp[a][b-1][c-1] == 0)
+		{
+			dp[a][b-1][c-1] = w(a, b-1 ,c-1);
+		}
+		if(dp[a][b-1][c] == 0)
+		{
+			dp[a][b-1][c] = w(a, b-1, c);
+		}
+		dp[a][b][c] = dp[a][b][c-1] + dp[a][b-1][c-1] - dp[a][b-1][c];
+	}
+	
+	else//同上
+	{
+		if(dp[a-1][b][c] == 0)
+		{
+			dp[a-1][b][c] = w(a-1, b, c);
+		}
+		if(dp[a-1][b-1][c] == 0)
+		{
+			dp[a-1][b-1][c] = w(a-1, b-1 ,c);
+		}
+		if(dp[a-1][b][c-1] == 0)
+		{
+			dp[a-1][b][c-1] = w(a-1, b, c-1);
+		}
+		if(dp[a-1][b-1][c-1] == 0)
+		{
+			dp[a-1][b-1][c-1] = w(a-1, b-1, c-1);
+		}
+		dp[a][b][c] = dp[a-1][b][c] + dp[a-1][b][c-1] + dp[a-1][b-1][c] - dp[a-1][b-1][c-1];
+	}
+	
+	return dp[a][b][c];
 }
-int dp[25][25][25];
-int n, m, x, y, z, ans;
-int check1(int a, int b, int c)
+
+int main()
 {
-  int ret;
-  if (a <= 0 || b <= 0 || c <= 0)
-    dp[a][b][c]=1;
-  else if (a > 20 || b > 20 || c > 20)
-    return check1(20, 20, 20);
-  else if (a < b && b < c)
-    return check1(a, b, c - 1) + check1(a, b - 1, c - 1) - check1(a, b - 1, c);
-  else
-    return check1(a - 1, b, c) + check1(a - 1, b, c - 1) + check1(a - 1, b - 1, c) - check1(a - 1, b - 1, c - 1);
-  return 0;
-}
-signed main()
-{
-  ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-  vector<int> a(N);
-  vector<int> b(N);
-  vector<int> c(N);
-  int i = 0;
-  while (1)
-  {
-    cin >> x >> y >> z;
-    if (x == -1 && y == -1 && z == -1)
-      break;
-    a[i] = x;
-    b[i] = y;
-    c[i++] = z;
-    
-    cout << "w" << "(" << x << ", " << y << ", " << z << ')' << " = " << check1(x, y, z) << en;
-  }
-  return 0;
+	LL a, b, c;
+	
+	while(scanf("%lld%lld%lld", &a, &b, &c))//无限输入，直到“-1 -1 -1”
+	{
+		if(a == -1 && b == -1 && c == -1) return 0;//-1 -1 -1就直接结束，不运算了。
+		
+		printf("w(%lld, %lld, %lld) = ", a, b, c);
+		printf("%lld\n", w(a, b, c));
+	}
 }
